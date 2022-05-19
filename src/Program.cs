f401364403fc9bbd30208bebbe658698b26e89b6
Sukhrob Ilyosbekov <suxrobgm@gmail.com>
2022-05-19 16:36:11 -0400

    added solution of the 5th problem

--- src/Program.cs
+++ src/Program.cs
@@ -2,6 +2,5 @@
 
 var solution = new Solution();
 
-int[] nums1 = { 1, 3 };
-int[] nums2 = { 2 };
-var output = solution.FindMedianSortedArrays(nums1, nums2);
+var output = solution.LongestPalindrome("babad");
+Console.WriteLine(output);
---
+++ src/Solutions/Solution5.cs
@@ -0,0 +1,72 @@
+﻿using System.Text;
+
+namespace LeetCode.Solutions;
+
+public partial class Solution
+{
+    /// <summary>
+    /// Given a string s, return the longest palindromic substring in s.
+    /// <see href="https://leetcode.com/problems/longest-palindromic-substring/">See the problem</see>
+    /// </summary>
+    /// <remarks>Best case O(n), worst case O(n^3)</remarks>
+    public string LongestPalindrome(string s)
+    {
+        var len = s.Length;
+
+        if (len == 1)
+        {
+            return s;
+        }
+
+        // Check string is palindrome itself
+        if (IsPalindrome(s))
+        {
+            return s;
+        }
+
+        // Total possible substrings for string of size n is n*(n+1)/2  
+        var res = "";
+        var maxLen = 0;
+
+        for (var i = 0; i < len; i++)
+        {
+            for (var j = 0; j < len - i; j++)
+            {
+                var substr = s.Substring(i, j + 1);      
+
+                if (IsPalindrome(substr))
+                {
+                    if (substr.Length > maxLen)
+                    {
+                        res = substr;
+                        maxLen = substr.Length;
+                    }
+                }
+            }
+        }
+
+        return res;
+    }
+
+    private bool IsPalindrome(string str)
+    {
+        var len = str.Length;
+        var isPalindrome = true;
+
+        if (len == 1)
+        {
+            return isPalindrome;
+        }
+
+        for (var i = 0; i < len / 2; i++)
+        {
+            if (str[i] != str[len - i - 1])
+            {
+                isPalindrome = false;
+                break;
+            }
+        }
+
+        return isPalindrome;
+    }
+}
