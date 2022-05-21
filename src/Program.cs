5614944a800a5bea8991b3738189e435cf1cabf8
Sukhrob Ilyosbekov <suxrobgm@gmail.com>
2022-05-21 18:48:37 -0400

    solution 35

--- src/Program.cs
+++ src/Program.cs
@@ -3,6 +3,6 @@ using LeetCode.Solutions;
 
 var solution = new Solution();
 
-var output = solution.StrStr("mississippi", "issipi");
+var output = solution.SearchInsert(new[] {1, 3, 5, 6}, 5);
 Console.WriteLine(output);
 Console.ReadLine();
---
+++ src/Solutions/Solution35.cs
@@ -0,0 +1,48 @@
+﻿namespace LeetCode.Solutions;
+
+public partial class Solution
+{
+    /// <summary>
+    /// Given a sorted array of distinct integers and a target value, return the index if the target is found. If not, return the index where it would be if it were inserted in order.
+    /// You must write an algorithm with O(log n) runtime complexity.
+    /// <see href="https://leetcode.com/problems/search-insert-position/">See the problem</see>
+    /// </summary>
+    public int SearchInsert(int[] nums, int target)
+    {
+        if (target > nums[nums.Length - 1]) 
+            return nums.Length;
+
+        if (target < nums[0]) 
+            return 0;
+
+        int index = BinarySearch(nums, target);
+        return index;
+    }
+
+    private int BinarySearch(int[] nums, int target)
+    {
+        int left = 0;
+        int right = nums.Length - 1;
+        
+        while (left <= right)
+        {
+            int middle = (left + right) / 2;
+
+            if (nums[middle] == target)
+            {
+                return middle;
+            }
+            else if (nums[middle] > target)
+            {
+                right = middle - 1;
+                
+            }
+            else
+            {
+                left = middle + 1;
+            }
+        }
+
+        return left;
+    }
+}
