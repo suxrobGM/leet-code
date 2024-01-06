3fea6158b8d8f59490866fa1c6bf4dcac7cab632
Sukhrob Ilyosbekov <suxrobgm@gmail.com>
2024-01-05 23:49:32 -0500

    added solution #11

--- src/CSharp/Program.cs
+++ src/CSharp/Program.cs
@@ -1,7 +1,7 @@
 ﻿using LeetCode.Solutions;
 
-var solution = new Solution8();
+var solution = new Solution11();
 
-var output = solution.MyAtoi("18446744073709551617");
+var output = solution.MaxArea([1,8,6,2,5,4,8,3,7]);
 Console.WriteLine(output);
 Console.ReadLine();
---
+++ src/CSharp/Solutions/Solution11.cs
@@ -0,0 +1,34 @@
+﻿namespace LeetCode.Solutions;
+
+public class Solution11
+{
+    /// <summary>
+    /// Problem #11
+    /// <a href="https://leetcode.com/problems/container-with-most-water/">See the problem</a>
+    /// </summary>
+    public int MaxArea(int[] height)
+    {
+        var maxArea = 0;
+        var left = 0;
+        var right = height.Length - 1;
+
+        while (left < right)
+        {
+            var width = right - left;
+            var minHeight = Math.Min(height[left], height[right]);
+            maxArea = Math.Max(maxArea, width * minHeight);
+
+            // Move the pointer pointing to the shorter line towards the other end
+            if (height[left] < height[right])
+            {
+                left++;
+            }
+            else
+            {
+                right--;
+            }
+        }
+
+        return maxArea;
+    }
+}
