958e9166db34597cdba65a259ad353a047fd88f6
Sukhrob Ilyosbekov <suxrobgm@gmail.com>
2024-01-12 23:41:06 -0500

    added problem #

--- src/CSharp/Program.cs
+++ src/CSharp/Program.cs
@@ -1,13 +1,7 @@
-﻿using LeetCode.DataStructures;
-using LeetCode.Solutions;
+﻿using LeetCode.Solutions;
 
-var solution = new Solution19();
+var solution = new Solution22();
 
-var head = new ListNode(1);
-head.next = new ListNode(2);
-head.next.next = new ListNode(3);
-head.next.next.next = new ListNode(4);
-head.next.next.next.next = new ListNode(5);
-var output = solution.RemoveNthFromEnd(head, 2);
+var output = solution.GenerateParenthesis(3);
 Console.WriteLine(output);
 Console.ReadLine();
---
+++ src/CSharp/Solutions/Solution22.cs
@@ -0,0 +1,36 @@
+﻿namespace LeetCode.Solutions;
+
+public class Solution22
+{
+    /// <summary>
+    /// 22. Generate Parentheses
+    /// <a href="https://leetcode.com/problems/generate-parentheses">See the problem</a>
+    /// </summary>
+    public IList<string> GenerateParenthesis(int n)
+    {
+        var result = new List<string>();
+        var queue = new Queue<(string str, int open, int close)>();
+        queue.Enqueue(("", 0, 0));
+
+        while (queue.Count > 0)
+        {
+            var (str, open, close) = queue.Dequeue();
+            if (str.Length == n * 2)
+            {
+                result.Add(str);
+                continue;
+            }
+
+            if (open < n)
+            {
+                queue.Enqueue((str + "(", open + 1, close));
+            }
+            if (close < open)
+            {
+                queue.Enqueue((str + ")", open, close + 1));
+            }
+        }
+
+        return result;
+    }
+}
