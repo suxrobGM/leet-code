6cddae7e6a898dde198a2d720b42220722a8e77c
Sukhrob Ilyosbekov <suxrobgm@gmail.com>
2024-01-21 22:21:16 -0500

    added solution #169

---
+++ src/CSharp/Solutions/Solution169.cs
@@ -0,0 +1,24 @@
+﻿namespace LeetCode.Solutions;
+
+public class Solution169
+{
+    /// <summary>
+    /// 169. Majority Element
+    /// <a href="https://leetcode.com/problems/majority-element">See the problem</a>
+    /// </summary>
+    public int MajorityElement(int[] nums)
+    {
+        var frequencies = new Dictionary<int, int>();
+        
+        foreach (var num in nums)
+        {
+            if (!frequencies.TryAdd(num, 1))
+            {
+                frequencies[num]++;
+            }
+        }
+
+        var majorityElement = frequencies.MaxBy(pair => pair.Value).Key;
+        return majorityElement;
+    }
+}
