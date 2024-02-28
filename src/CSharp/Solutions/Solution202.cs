67b15408b9cdb2b291a6b9713781587c8d6bb4cc
Sukhrob Ilyosbekov <suxrobgm@gmail.com>
2024-02-27 19:23:56 -0500

    added solution #202

---
+++ src/CSharp/Solutions/Solution202.cs
@@ -0,0 +1,34 @@
+﻿namespace LeetCode.Solutions;
+
+public class Solution202
+{
+    /// <summary>
+    /// 202. Happy Number - Easy
+    /// <a href="https://leetcode.com/problems/happy-number">See the problem</a>
+    /// </summary>
+    public bool IsHappy(int n)
+    {
+        var slow = n;
+        var fast = SumOfSquares(n);
+        
+        while (fast != 1 && slow != fast)
+        {
+            slow = SumOfSquares(slow);
+            fast = SumOfSquares(SumOfSquares(fast));
+        }
+        
+        return fast == 1;
+    }
+    
+    private int SumOfSquares(int n)
+    {
+        var sum = 0;
+        while (n > 0)
+        {
+            var digit = n % 10;
+            sum += digit * digit;
+            n /= 10;
+        }
+        return sum;
+    }
+}
