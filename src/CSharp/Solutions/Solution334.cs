aa788b7af6a27df157cf00ed76becc90e925cb69
Sukhrob Ilyosbekov <suxrobgm@gmail.com>
2024-03-12 17:25:54 -0400

    added Solution334

---
+++ src/CSharp/Solutions/Solution334.cs
@@ -0,0 +1,32 @@
+﻿namespace LeetCode.Solutions;
+
+public class Solution334
+{
+    /// <summary>
+    /// 334. Increasing Triplet Subsequence - Medium
+    /// <a href="https://leetcode.com/problems/increasing-triplet-subsequence">See the problem</a>
+    /// </summary>
+    public bool IncreasingTriplet(int[] nums)
+    {
+        var first = int.MaxValue;
+        var second = int.MaxValue;
+
+        foreach (var num in nums)
+        {
+            if (num <= first)
+            {
+                first = num;
+            }
+            else if (num <= second)
+            {
+                second = num;
+            }
+            else
+            {
+                return true;
+            }
+        }
+
+        return false;
+    }
+}
