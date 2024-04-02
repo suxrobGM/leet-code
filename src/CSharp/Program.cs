1cd6075452de48cb0b91ca76ad6bf60b772b11ad
Sukhrob Ilyosbekov <suxrobgm@gmail.com>
2024-04-02 17:37:37 -0400

    added solution 62

--- src/CSharp/Program.cs
+++ src/CSharp/Program.cs
@@ -6,7 +6,7 @@ public static class Program
     {
         BenchmarkHoare();
         BenchmarkLomuto();
-        BenchmarkLargeSize();
+        // BenchmarkLargeSize();
         Console.ReadLine();
     }
     
---
+++ src/CSharp/Solutions/Solution62.cs
@@ -0,0 +1,32 @@
+﻿using LeetCode.DataStructures;
+
+namespace LeetCode.Solutions;
+
+public class Solution62
+{
+    /// <summary>
+    /// 62. Unique Paths - Medium
+    /// <a href="https://leetcode.com/problems/unique-paths">See the problem</a>
+    /// </summary>
+    public int UniquePaths(int m, int n)
+    {
+        var dp = new int[m, n];
+        
+        for (var i = 0; i < m; i++)
+        {
+            for (var j = 0; j < n; j++)
+            {
+                if (i == 0 || j == 0)
+                {
+                    dp[i, j] = 1;
+                }
+                else
+                {
+                    dp[i, j] = dp[i - 1, j] + dp[i, j - 1];
+                }
+            }
+        }
+        
+        return dp[m - 1, n - 1];
+    }
+}
