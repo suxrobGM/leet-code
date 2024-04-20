ca8aba9f4c0c6187d994ce173dd3c44e8024c7af
Sukhrob Ilyosbekov <suxrobgm@gmail.com>
2024-04-20 07:40:46 -0400

    added solutions 127 and 887

---
+++ src/CSharp/Solutions/Solution127.cs
@@ -0,0 +1,67 @@
+﻿namespace LeetCode.Solutions;
+
+public class Solution127
+{
+    /// <summary>
+    /// 127. Word Ladder - Hard
+    /// <a href="https://leetcode.com/problems/word-ladder">See the problem</a>
+    /// </summary>
+    public int LadderLength(string beginWord, string endWord, IList<string> wordList)
+    {
+        var wordSet = new HashSet<string>(wordList);
+        if (!wordSet.Contains(endWord))
+        {
+            return 0;
+        }
+
+        var queue = new Queue<string>();
+        queue.Enqueue(beginWord);
+
+        var visited = new HashSet<string>
+        {
+            beginWord
+        };
+
+        var level = 1;
+
+        // BFS
+        while (queue.Count > 0)
+        {
+            var size = queue.Count;
+
+            for (var i = 0; i < size; i++)
+            {
+                var currentWord = queue.Dequeue();
+
+                if (currentWord == endWord)
+                {
+                    return level;
+                }
+
+                var charArray = currentWord.ToCharArray();
+
+                for (var j = 0; j < charArray.Length; j++)
+                {
+                    var originalChar = charArray[j];
+
+                    for (var k = 0; k < 26; k++)
+                    {
+                        charArray[j] = (char)('a' + k);
+                        var newWord = new string(charArray);
+
+                        if (wordSet.Contains(newWord) && visited.Add(newWord))
+                        {
+                            queue.Enqueue(newWord);
+                        }
+                    }
+
+                    charArray[j] = originalChar;
+                }
+            }
+
+            level++;
+        }
+
+        return 0;
+    }
+}
---
+++ src/CSharp/Solutions/Solution887.cs
@@ -0,0 +1,25 @@
+﻿namespace LeetCode.Solutions;
+
+public class Solution887
+{
+    /// <summary>
+    /// 887. Super Egg Drop - Hard
+    /// <a href="https://leetcode.com/problems/super-egg-drop">See the problem</a>
+    /// </summary>
+    public int SuperEggDrop(int k, int n)
+    {
+        var dp = new int[k + 1, n + 1];
+        var m = 0; // Number of moves
+
+        while (dp[k, m] < n)
+        {
+            m++;
+            for (var i = 1; i <= k; i++)
+            {
+                dp[i, m] = dp[i, m - 1] + dp[i - 1, m - 1] + 1;
+            }
+        }
+
+        return m;
+    }
+}
