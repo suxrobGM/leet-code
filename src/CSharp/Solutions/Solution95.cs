302676962ea81d2514095559e59ff6d03881b962
Sukhrob Ilyosbekov <suxrobgm@gmail.com>
2024-04-26 19:24:54 -0400

    added solutions 95 96

---
+++ src/CSharp/Solutions/Solution95.cs
@@ -0,0 +1,53 @@
+﻿using LeetCode.DataStructures;
+
+namespace LeetCode.Solutions;
+
+public class Solution95
+{
+    /// <summary>
+    /// 95. Unique Binary Search Trees II - Medium
+    /// <a href="https://leetcode.com/problems/unique-binary-search-trees-ii">See the problem</a>
+    /// </summary>
+    public IList<TreeNode> GenerateTrees(int n)
+    {
+        if (n == 0)
+        {
+            return new List<TreeNode>();
+        }
+
+        return GenerateTrees(1, n);
+    }
+    
+    private IList<TreeNode> GenerateTrees(int start, int end)
+    {
+        var result = new List<TreeNode>();
+
+        if (start > end)
+        {
+            result.Add(null);
+            return result;
+        }
+
+        for (var i = start; i <= end; i++)
+        {
+            var leftTrees = GenerateTrees(start, i - 1);
+            var rightTrees = GenerateTrees(i + 1, end);
+
+            foreach (var left in leftTrees)
+            {
+                foreach (var right in rightTrees)
+                {
+                    var root = new TreeNode(i)
+                    {
+                        left = left,
+                        right = right
+                    };
+
+                    result.Add(root);
+                }
+            }
+        }
+
+        return result;
+    }
+}
---
+++ src/CSharp/Solutions/Solution96.cs
@@ -0,0 +1,27 @@
+﻿using LeetCode.DataStructures;
+
+namespace LeetCode.Solutions;
+
+public class Solution96
+{
+    /// <summary>
+    /// 96. Unique Binary Search Trees - Medium
+    /// <a href="https://leetcode.com/problems/unique-binary-search-trees">See the problem</a>
+    /// </summary>
+    public int NumTrees(int n)
+    {
+        var dp = new int[n + 1];
+        dp[0] = 1;
+        dp[1] = 1;
+
+        for (var i = 2; i <= n; i++)
+        {
+            for (var j = 1; j <= i; j++)
+            {
+                dp[i] += dp[j - 1] * dp[i - j];
+            }
+        }
+
+        return dp[n];
+    }
+}
