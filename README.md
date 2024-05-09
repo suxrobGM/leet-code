f66c0017f6cfbea37151bad712638346b564f958
Sukhrob Ilyosbekov <suxrobgm@gmail.com>
2024-05-09 16:33:10 -0400

    added solution 149

--- README.md
+++ README.md
@@ -129,6 +129,7 @@ LeetCode problems and solutions
 | 134  | [Gas Station](https://leetcode.com/problems/gas-station) | [C#](./src/CSharp/Solutions/Solution134.cs) | Medium |
 | 135  | [Candy](https://leetcode.com/problems/candy) | [C#](./src/CSharp/Solutions/Solution135.cs) | Hard |
 | 140  | [Word Break II](https://leetcode.com/problems/word-break-ii) | [C#](./src/CSharp/Solutions/Solution140.cs) | Hard |
+| 149 | [Max Points on a Line](https://leetcode.com/problems/max-points-on-a-line) | [C#](./src/CSharp/Solutions/Solution149.cs) | Hard |
 | 151  | [Reverse Words in a String](https://leetcode.com/problems/reverse-words-in-a-string) | [C#](./src/CSharp/Solutions/Solution151.cs) | Medium |
 | 167  | [Two Sum II - Input Array Is Sorted](https://leetcode.com/problems/two-sum-ii-input-array-is-sorted) | [C#](./src/CSharp/Solutions/Solution167.cs) | Medium |
 | 169  | [Majority Element](https://leetcode.com/problems/majority-element) | [C#](./src/CSharp/Solutions/Solution169.cs) | Easy |
---
+++ src/CSharp/Solutions/Solution149.cs
@@ -0,0 +1,80 @@
+﻿namespace LeetCode.Solutions;
+
+public class Solution149
+{
+    /// <summary>
+    /// 149. Max Points on a Line - Hard
+    /// <a href="https://leetcode.com/problems/max-points-on-a-line">See the problem</a>
+    /// </summary>
+    public int MaxPoints(int[][] points)
+    {
+        if (points.Length < 3)
+        {
+            return points.Length;
+        }
+        
+        var max = 0;
+        
+        for (var i = 0; i < points.Length; i++)
+        {
+            var slopes = new Dictionary<string, int>();
+            var duplicates = 0;
+            var localMax = 0;
+            
+            for (var j = i + 1; j < points.Length; j++)
+            {
+                if (points[i][0] == points[j][0] && points[i][1] == points[j][1])
+                {
+                    duplicates++;
+                    continue;
+                }
+                
+                var slope = CalculateSlope(points[i], points[j]);
+                var key = slope.Item1 + "/" + slope.Item2;
+                
+                if (!slopes.ContainsKey(key))
+                {
+                    slopes[key] = 0;
+                }
+                
+                slopes[key]++;
+                localMax = Math.Max(localMax, slopes[key]);
+            }
+            
+            max = Math.Max(max, localMax + duplicates + 1);
+        }
+        
+        return max;
+    }
+    
+    private (int, int) CalculateSlope(int[] point1, int[] point2)
+    {
+        var dx = point2[0] - point1[0];
+        var dy = point2[1] - point1[1];
+        
+        if (dx == 0)
+        {
+            return (0, 1);
+        }
+        
+        if (dy == 0)
+        {
+            return (1, 0);
+        }
+        
+        var gcd = GCD(dx, dy);
+        return (dx / gcd, dy / gcd);
+    }
+    
+    private int GCD(int a, int b)
+    {
+        while (b != 0)
+        {
+            var temp = b;
+            b = a % b;
+            a = temp;
+        }
+        
+        return a;
+    }
+}
