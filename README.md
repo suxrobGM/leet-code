77f9deb4ec039910bffdc8ac968f7caf70934b31
Sukhrob Ilyosbekov <suxrobgm@gmail.com>
2024-05-16 22:33:10 -0400

    added solution 146

--- README.md
+++ README.md
@@ -135,6 +135,7 @@ LeetCode problems and solutions
 | 134  | [Gas Station](https://leetcode.com/problems/gas-station) | [C#](./src/CSharp/Solutions/Solution134.cs) | Medium |
 | 135  | [Candy](https://leetcode.com/problems/candy) | [C#](./src/CSharp/Solutions/Solution135.cs) | Hard |
 | 140  | [Word Break II](https://leetcode.com/problems/word-break-ii) | [C#](./src/CSharp/Solutions/Solution140.cs) | Hard |
+| 146 | [LRU Cache](https://leetcode.com/problems/lru-cache) | [C#](./src/CSharp/Solutions/Solution146.cs) | Medium |
 | 149  | [Max Points on a Line](https://leetcode.com/problems/max-points-on-a-line) | [C#](./src/CSharp/Solutions/Solution149.cs) | Hard |
 | 150  | [Evaluate Reverse Polish Notation](https://leetcode.com/problems/evaluate-reverse-polish-notation) | [C#](./src/CSharp/Solutions/Solution150.cs) | Medium |
 | 151  | [Reverse Words in a String](https://leetcode.com/problems/reverse-words-in-a-string) | [C#](./src/CSharp/Solutions/Solution151.cs) | Medium |
---
+++ src/CSharp/Solutions/Solution146.cs
@@ -0,0 +1,57 @@
+﻿namespace LeetCode.Solutions;
+
+public class Solution146
+{
+    /// <summary>
+    /// 146. LRU Cache - Medium
+    /// <a href="https://leetcode.com/problems/lru-cache">See the problem</a>
+    /// </summary>
+    public class LRUCache 
+    {
+        private readonly int _capacity;
+        private readonly Dictionary<int, LinkedListNode<(int, int)>> _cache;
+        private readonly LinkedList<(int, int)> _list;
+
+        public LRUCache(int capacity) 
+        {
+            _capacity = capacity;
+            _cache = new Dictionary<int, LinkedListNode<(int, int)>>();
+            _list = [];
+        }
+
+        public int Get(int key) 
+        {
+            if (!_cache.TryGetValue(key, out var node))
+            {
+                return -1;
+            }
+
+            _list.Remove(node);
+            _list.AddFirst(node);
+
+            return node.Value.Item2;
+        }
+
+        public void Put(int key, int value) 
+        {
+            if (_cache.TryGetValue(key, out var node1))
+            {
+                _list.Remove(node1);
+                _list.AddFirst(node1);
+                node1.Value = (key, value);
+            }
+            else
+            {
+                if (_cache.Count == _capacity)
+                {
+                    _cache.Remove(_list.Last!.Value.Item1);
+                    _list.RemoveLast();
+                }
+
+                var node = new LinkedListNode<(int, int)>((key, value));
+                _cache[key] = node;
+                _list.AddFirst(node);
+            }
+        }
+    }
+}
