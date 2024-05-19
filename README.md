5f8b450fc450070ea8ed9db12595399a6ff81fdb
Sukhrob Ilyosbekov <suxrobgm@gmail.com>
2024-05-19 00:40:12 -0400

    added solution 143

--- README.md
+++ README.md
@@ -136,6 +136,7 @@ LeetCode problems and solutions
 | 135  | [Candy](https://leetcode.com/problems/candy) | [C#](./src/CSharp/Solutions/Solution135.cs) | Hard |
 | 138  | [Copy List with Random Pointer](https://leetcode.com/problems/copy-list-with-random-pointer) | [C#](./src/CSharp/Solutions/Solution138.cs) | Medium |
 | 140  | [Word Break II](https://leetcode.com/problems/word-break-ii) | [C#](./src/CSharp/Solutions/Solution140.cs) | Hard |
+| 143  | [Reorder List](https://leetcode.com/problems/reorder-list) | [C#](./src/CSharp/Solutions/Solution143.cs) | Medium |
 | 146  | [LRU Cache](https://leetcode.com/problems/lru-cache) | [C#](./src/CSharp/Solutions/Solution146.cs) | Medium |
 | 149  | [Max Points on a Line](https://leetcode.com/problems/max-points-on-a-line) | [C#](./src/CSharp/Solutions/Solution149.cs) | Hard |
 | 150  | [Evaluate Reverse Polish Notation](https://leetcode.com/problems/evaluate-reverse-polish-notation) | [C#](./src/CSharp/Solutions/Solution150.cs) | Medium |
---
+++ src/CSharp/Solutions/Solution143.cs
@@ -0,0 +1,49 @@
+﻿using LeetCode.DataStructures;
+
+namespace LeetCode.Solutions;
+
+public class Solution143
+{
+    /// <summary>
+    /// 143. Reorder List - Medium
+    /// <a href="https://leetcode.com/problems/reorder-list">See the problem</a>
+    /// </summary>
+    public void ReorderList(ListNode head)
+    {
+        if (head?.next == null)
+        {
+            return;
+        }
+
+        // Step 1: Find the middle of the linked list
+        ListNode slow = head, fast = head;
+        while (fast is { next: not null })
+        {
+            slow = slow.next;
+            fast = fast.next.next;
+        }
+
+        // Step 2: Reverse the second half
+        ListNode prev = null, curr = slow, temp;
+        while (curr != null)
+        {
+            temp = curr.next;
+            curr.next = prev;
+            prev = curr;
+            curr = temp;
+        }
+
+        // Step 3: Merge the two halves
+        ListNode first = head, second = prev;
+        while (second?.next != null)
+        {
+            temp = first.next;
+            first.next = second;
+            first = temp;
+
+            temp = second.next;
+            second.next = first;
+            second = temp;
+        }
+    }
+}
