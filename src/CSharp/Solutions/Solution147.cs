de8e9ad308207478030db9f877328534b3b2f17c
Sukhrob Ilyosbekov <suxrobgm@gmail.com>
2024-05-20 18:26:11 -0400

    added solution 147

---
+++ src/CSharp/Solutions/Solution147.cs
@@ -0,0 +1,44 @@
+﻿using LeetCode.DataStructures;
+
+namespace LeetCode.Solutions;
+
+public class Solution147
+{
+    /// <summary>
+    /// 147. Insertion Sort List - Medium
+    /// <a href="https://leetcode.com/problems/insertion-sort-list">See the problem</a>
+    /// </summary>
+    public ListNode InsertionSortList(ListNode head)
+    {
+        if (head?.next == null)
+        {
+            return head;
+        }
+
+        var dummy = new ListNode();
+        var prev = dummy;
+        var curr = head;
+
+        while (curr != null)
+        {
+            var next = curr.next;
+
+            if (prev.next != null && prev.next.val > curr.val)
+            {
+                prev = dummy;
+            }
+
+            while (prev.next != null && prev.next.val < curr.val)
+            {
+                prev = prev.next;
+            }
+
+            curr.next = prev.next;
+            prev.next = curr;
+
+            curr = next;
+        }
+
+        return dummy.next;
+    }
+}
