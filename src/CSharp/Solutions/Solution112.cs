9df26cfaf73c934e82f5a35ea265aae920d14fdc
Sukhrob Ilyosbekov <suxrobgm@gmail.com>
2024-05-23 00:12:02 -0400

    added solution 112

---
+++ src/CSharp/Solutions/Solution112.cs
@@ -0,0 +1,25 @@
+﻿using LeetCode.DataStructures;
+
+namespace LeetCode.Solutions;
+
+public class Solution112
+{
+    /// <summary>
+    /// 112. Path Sum - Easy
+    /// <a href="https://leetcode.com/problems/path-sum">See the problem</a>
+    /// </summary>
+    public bool HasPathSum(TreeNode root, int targetSum)
+    {
+        if (root == null)
+        {
+            return false;
+        }
+        
+        if (root.left == null && root.right == null)
+        {
+            return targetSum == root.val;
+        }
+        
+        return HasPathSum(root.left, targetSum - root.val) || HasPathSum(root.right, targetSum - root.val);
+    }
+}
