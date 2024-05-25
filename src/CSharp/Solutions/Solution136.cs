3f49b53465491d79cdedb6f09066251c577e6d24
Sukhrob Ilyosbekov <suxrobgm@gmail.com>
2024-05-25 16:02:33 -0400

    added solution 136

---
+++ src/CSharp/Solutions/Solution136.cs
@@ -0,0 +1,19 @@
+﻿namespace LeetCode.Solutions;
+
+public class Solution136
+{
+    /// <summary>
+    /// 136. Single Number - Easy
+    /// <a href="https://leetcode.com/problems/single-number">See the problem</a>
+    /// </summary>
+    public int SingleNumber(int[] nums)
+    {
+        var result = 0;
+        foreach (var num in nums)
+        {
+            result ^= num;
+        }
+
+        return result;
+    }
+}
