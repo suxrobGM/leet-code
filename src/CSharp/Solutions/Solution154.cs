f74febc2898f9bf5729beb3aab39953e4aad7c1c
Sukhrob Ilyosbekov <suxrobgm@gmail.com>
2024-06-01 20:49:47 -0400

    added solution 154

---
+++ src/CSharp/Solutions/Solution154.cs
@@ -0,0 +1,34 @@
+﻿namespace LeetCode.Solutions;
+
+public class Solution154
+{
+    /// <summary>
+    /// 154. Find Minimum in Rotated Sorted Array II - Hard
+    /// <a href="https://leetcode.com/problems/find-minimum-in-rotated-sorted-array-ii">See the problem</a>
+    /// </summary>
+    public int FindMin(int[] nums)
+    {
+        var left = 0;
+        var right = nums.Length - 1;
+
+        while (left < right)
+        {
+            var mid = left + (right - left) / 2;
+
+            if (nums[mid] < nums[right])
+            {
+                right = mid;
+            }
+            else if (nums[mid] > nums[right])
+            {
+                left = mid + 1;
+            }
+            else
+            {
+                right--;
+            }
+        }
+
+        return nums[left];
+    }
+}
