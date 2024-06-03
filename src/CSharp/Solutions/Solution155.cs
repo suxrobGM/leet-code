a44fe0c87921b2788ad3f47e8135dd0764ff13f1
Sukhrob Ilyosbekov <suxrobgm@gmail.com>
2024-06-02 22:32:21 -0400

    added solution 155

---
+++ src/CSharp/Solutions/Solution155.cs
@@ -0,0 +1,49 @@
+﻿namespace LeetCode.Solutions;
+
+public class Solution155
+{
+    /// <summary>
+    /// 155. Min Stack - Medium
+    /// <a href="https://leetcode.com/problems/min-stack">See the problem</a>
+    /// </summary>
+    public class MinStack
+    {
+        private readonly Stack<int> _stack = new();
+        private readonly Stack<int> _minStack = new();
+
+        public void Push(int val)
+        {
+            _stack.Push(val);
+
+            if (_minStack.Count == 0 || val <= _minStack.Peek())
+            {
+                _minStack.Push(val);
+            }
+        }
+
+        public void Pop()
+        {
+            if (_stack.Count == 0)
+            {
+                return;
+            }
+
+            if (_stack.Peek() == _minStack.Peek())
+            {
+                _minStack.Pop();
+            }
+
+            _stack.Pop();
+        }
+
+        public int Top()
+        {
+            return _stack.Peek();
+        }
+
+        public int GetMin()
+        {
+            return _minStack.Peek();
+        }
+    }
+}
