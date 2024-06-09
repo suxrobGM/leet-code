d41ae7c7b6c5d9348bfae0bd3915c7cefaa3bdd5
Sukhrob Ilyosbekov <suxrobgm@gmail.com>
2024-06-08 21:15:06 -0400

    added solution 172

---
+++ src/CSharp/Solutions/Solution172.cs
@@ -0,0 +1,21 @@
+﻿namespace LeetCode.Solutions;
+
+public class Solution172
+{
+    /// <summary>
+    /// 172. Factorial Trailing Zeroes - Medium
+    /// <a href="https://leetcode.com/problems/factorial-trailing-zeroes">See the problem</a>
+    /// </summary>
+    public int TrailingZeroes(int n)
+    {
+        var result = 0;
+
+        while (n > 0)
+        {
+            n /= 5;
+            result += n;
+        }
+
+        return result;
+    }
+}
