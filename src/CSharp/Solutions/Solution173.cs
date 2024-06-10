d608faac842a5322e2de89e8b3ee34f27015c81a
Sukhrob Ilyosbekov <suxrobgm@gmail.com>
2024-06-09 20:59:22 -0400

    added solution 173

---
+++ src/CSharp/Solutions/Solution173.cs
@@ -0,0 +1,42 @@
+﻿using LeetCode.DataStructures;
+
+namespace LeetCode.Solutions;
+
+public class Solution173
+{
+    /// <summary>
+    /// 173. Binary Search Tree Iterator - Medium
+    /// <a href="https://leetcode.com/problems/binary-search-tree-iterator">See the problem</a>
+    /// </summary>
+    public class BSTIterator 
+    {
+        private readonly Stack<TreeNode> _stack = new();
+
+        public BSTIterator(TreeNode root)
+        {
+            FillStack(root);
+        }
+
+        public int Next()
+        {
+            var node = _stack.Pop();
+            FillStack(node.right);
+
+            return node.val;
+        }
+
+        public bool HasNext()
+        {
+            return _stack.Count > 0;
+        }
+
+        private void FillStack(TreeNode node)
+        {
+            while (node != null)
+            {
+                _stack.Push(node);
+                node = node.left;
+            }
+        }
+    }
+}
