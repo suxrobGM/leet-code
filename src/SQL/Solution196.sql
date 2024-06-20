9c57fe2a03d4c4803c50f9d3888cd89abee2ee48
Sukhrob Ilyosbekov <suxrobgm@gmail.com>
2024-06-19 21:02:48 -0400

    added solutions 196 197

---
+++ src/SQL/Solution196.sql
@@ -0,0 +1,10 @@
+﻿-- 196. Delete Duplicate Emails - Easy
+-- Link: https://leetcode.com/problems/delete-duplicate-emails
+-- Solution in PostgreSQL
+DELETE FROM Person
+WHERE id NOT IN
+(
+    SELECT MIN(id)
+    FROM Person
+    GROUP BY email
+);
---
+++ src/SQL/Solution197.sql
@@ -0,0 +1,12 @@
+﻿-- 197. Rising Temperature - Easy
+-- Link: https://leetcode.com/problems/rising-temperature
+-- Solution in PostgreSQL
+-- Solution in PostgreSQL
+SELECT weather.id AS "Id"
+FROM Weather
+WHERE Temperature > 
+(
+    SELECT Temperature 
+    FROM Weather AS PreviousDay
+    WHERE PreviousDay.RecordDate = Weather.RecordDate - INTERVAL '1 day'
+);
