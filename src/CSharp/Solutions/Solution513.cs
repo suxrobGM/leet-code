0b672592150298e71fa17c2c6fb53e8600eaf6ad
Sukhrob Ilyosbekov <SuxrobGM@gmail.com>
2024-09-01 14:43:24 -0400

    511 513

---
+++ src/CSharp/Solutions/Solution513.cs
@@ -0,0 +1,41 @@
+﻿using LeetCode.DataStructures;
+
+namespace LeetCode.Solutions;
+
+public class Solution513
+{
+    /// <summary>
+    /// 513. Find Bottom Left Tree Value - Medium
+    /// <a href="https://leetcode.com/problems/find-bottom-left-tree-value">See the problem</a>
+    /// </summary>
+    public int FindBottomLeftValue(TreeNode root)
+    {
+        var queue = new Queue<TreeNode>();
+        queue.Enqueue(root);
+
+        var result = 0;
+
+        while (queue.Count > 0)
+        {
+            var size = queue.Count;
+            result = queue.Peek().val;
+
+            for (var i = 0; i < size; i++)
+            {
+                var node = queue.Dequeue();
+
+                if (node.left != null)
+                {
+                    queue.Enqueue(node.left);
+                }
+
+                if (node.right != null)
+                {
+                    queue.Enqueue(node.right);
+                }
+            }
+        }
+
+        return result;
+    }
+}
---
+++ src/SQL/Solution511.sql
@@ -0,0 +1,10 @@
+﻿-- 511. Game Play Analysis I
+-- Link: https://leetcode.com/problems/game-play-analysis-i
+-- Solution in PostgreSQL
+SELECT 
+    player_id,
+    MIN(event_date) AS first_login
+FROM 
+    Activity
+GROUP BY 
+    player_id;
