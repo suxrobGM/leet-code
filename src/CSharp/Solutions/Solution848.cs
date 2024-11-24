139b64cecb3560eb27d4e1f849defab6e417de5f
Sukhrob Ilyosbekov <suxrobgm@gmail.com>
2024-11-24 10:01:16 +0000

    848

---
+++ src/CSharp/Solutions/Solution848.cs
@@ -0,0 +1,26 @@
+﻿using System.Text;
+using LeetCode.DataStructures;
+
+namespace LeetCode.Solutions;
+
+public class Solution848
+{
+    /// <summary>
+    /// 848. Shifting Letters - Medium
+    /// <a href="https://leetcode.com/problems/shifting-letters">See the problem</a>
+    /// </summary>
+    public string ShiftingLetters(string s, int[] shifts)
+    {
+        var n = s.Length;
+        var totalShifts = 0;
+        var sb = new StringBuilder(n);
+
+        for (var i = n - 1; i >= 0; i--)
+        {
+            totalShifts = (totalShifts + shifts[i]) % 26;
+            sb.Append((char)((s[i] - 'a' + totalShifts) % 26 + 'a'));
+        }
+
+        return string.Concat(sb.ToString().Reverse());
+    }
+}
