85447cb25cc13b16726f2ea13e30bd0971767e49
Sukhrob Ilyosbekov <sukhrob@emtechcarelabs.com>
2025-01-12 18:58:07 +0500

    940 941

---
+++ src/CSharp/Solutions/Solution940.cs
@@ -0,0 +1,41 @@
+﻿using System.Text;
+using LeetCode.DataStructures;
+
+namespace LeetCode.Solutions;
+
+public class Solution940
+{
+    /// <summary>
+    /// 940. Distinct Subsequences II - Hard
+    /// <a href="https://leetcode.com/problems/distinct-subsequences-ii">See the problem</a>
+    /// </summary>
+    public int DistinctSubseqII(string s)
+    {
+        const int MOD = 1000000007;
+        int n = s.Length;
+        var dp = new int[n + 1];
+        dp[0] = 1;
+
+        var last = new int[26];
+        Array.Fill(last, -1);
+
+        for (int i = 1; i <= n; i++)
+        {
+            dp[i] = dp[i - 1] * 2 % MOD;
+            if (last[s[i - 1] - 'a'] != -1)
+            {
+                dp[i] -= dp[last[s[i - 1] - 'a'] - 1];
+            }
+            dp[i] %= MOD;
+            last[s[i - 1] - 'a'] = i;
+        }
+
+        dp[n]--;
+        if (dp[n] < 0)
+        {
+            dp[n] += MOD;
+        }
+
+        return dp[n];
+    }
+}
---
+++ src/CSharp/Solutions/Solution941.cs
@@ -0,0 +1,34 @@
+﻿using System.Text;
+using LeetCode.DataStructures;
+
+namespace LeetCode.Solutions;
+
+public class Solution941
+{
+    /// <summary>
+    /// 941. Valid Mountain Array - Easy
+    /// <a href="https://leetcode.com/problems/valid-mountain-array">See the problem</a>
+    /// </summary>
+    public bool ValidMountainArray(int[] arr)
+    {
+        int n = arr.Length;
+        int i = 0;
+
+        while (i + 1 < n && arr[i] < arr[i + 1])
+        {
+            i++;
+        }
+
+        if (i == 0 || i == n - 1)
+        {
+            return false;
+        }
+
+        while (i + 1 < n && arr[i] > arr[i + 1])
+        {
+            i++;
+        }
+
+        return i == n - 1;
+    }
+}
