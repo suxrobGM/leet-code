24e48251b4cead64b019c3f3e97ff89174252687
Sukhrob Ilyosbekov <suxrobgm@gmail.com>
2025-01-22 16:19:37 -0500

    950 951

---
+++ src/CSharp/Solutions/Solution950.cs
@@ -0,0 +1,40 @@
+﻿using System.Text;
+using LeetCode.DataStructures;
+
+namespace LeetCode.Solutions;
+
+public class Solution950
+{
+    /// <summary>
+    /// 950. Reveal Cards In Increasing Order - Medium
+    /// <a href="https://leetcode.com/problems/reveal-cards-in-increasing-order">See the problem</a>
+    /// </summary>
+    public int[] DeckRevealedIncreasing(int[] deck)
+    {
+        // Sort the deck in ascending order
+        Array.Sort(deck);
+
+        // Use a queue to simulate the revealing process
+        var indexQueue = new Queue<int>();
+        for (int i = 0; i < deck.Length; i++)
+        {
+            indexQueue.Enqueue(i);
+        }
+
+        var result = new int[deck.Length];
+
+        foreach (var card in deck)
+        {
+            // Place the current card at the index from the queue
+            result[indexQueue.Dequeue()] = card;
+
+            // If there are still indices in the queue, move the next index to the back
+            if (indexQueue.Count > 0)
+            {
+                indexQueue.Enqueue(indexQueue.Dequeue());
+            }
+        }
+
+        return result;
+    }
+}
---
+++ src/CSharp/Solutions/Solution951.cs
@@ -0,0 +1,27 @@
+﻿using System.Text;
+using LeetCode.DataStructures;
+
+namespace LeetCode.Solutions;
+
+public class Solution951
+{
+    /// <summary>
+    /// 951. Flip Equivalent Binary Trees - Medium
+    /// <a href="https://leetcode.com/problems/flip-equivalent-binary-trees">See the problem</a>
+    /// </summary>
+    public bool FlipEquiv(TreeNode root1, TreeNode root2)
+    {
+        if (root1 == null && root2 == null)
+        {
+            return true;
+        }
+
+        if (root1 == null || root2 == null || root1.val != root2.val)
+        {
+            return false;
+        }
+
+        return (FlipEquiv(root1.left, root2.left) && FlipEquiv(root1.right, root2.right)) ||
+               (FlipEquiv(root1.left, root2.right) && FlipEquiv(root1.right, root2.left));
+    }
+}
