08be66b6e98ffc85cd73015c200d434f70425dd6
Sukhrob Ilyosbekov <suxrobgm@gmail.com>
2025-02-01 16:28:22 -0500

    968 969

---
+++ src/CSharp/Solutions/Solution968.cs
@@ -0,0 +1,47 @@
+﻿using System.Text;
+using LeetCode.DataStructures;
+
+namespace LeetCode.Solutions;
+
+public class Solution968
+{
+    private int cameras = 0;
+
+    /// <summary>
+    /// 968. Binary Tree Cameras - Hard
+    /// <a href="https://leetcode.com/problems/binary-tree-cameras">See the problem</a>
+    /// </summary>
+    public int MinCameraCover(TreeNode root)
+    {
+        // If root is uncovered, place a camera at root
+        if (DFS(root) == 0)
+        {
+            cameras++;
+        }
+        return cameras;
+    }
+
+    private int DFS(TreeNode node)
+    {
+        if (node == null) return 1; // Null nodes are already covered
+
+        int left = DFS(node.left);
+        int right = DFS(node.right);
+
+        // If any child is not covered, place a camera here
+        if (left == 0 || right == 0)
+        {
+            cameras++;
+            return 2; // This node has a camera
+        }
+
+        // If either child has a camera, this node is covered
+        if (left == 2 || right == 2)
+        {
+            return 1; // Covered but no camera
+        }
+
+        // If children are covered but have no camera, this node is not covered
+        return 0;
+    }
+}
---
+++ src/CSharp/Solutions/Solution969.cs
@@ -0,0 +1,56 @@
+﻿using System.Text;
+using LeetCode.DataStructures;
+
+namespace LeetCode.Solutions;
+
+public class Solution969
+{
+    private int cameras = 0;
+
+    /// <summary>
+    /// 969. Pancake Sorting - Medium
+    /// <a href="https://leetcode.com/problems/pancake-sorting">See the problem</a>
+    /// </summary>
+    public IList<int> PancakeSort(int[] arr)
+    {
+        var result = new List<int>();
+        PancakeSort(arr, arr.Length, result);
+        return result;
+    }
+
+    private void PancakeSort(int[] arr, int n, List<int> result)
+    {
+        if (n == 1) return;
+
+        var maxIndex = 0;
+        var max = arr[0];
+        for (var i = 1; i < n; i++)
+        {
+            if (arr[i] > max)
+            {
+                max = arr[i];
+                maxIndex = i;
+            }
+        }
+
+        if (maxIndex != n - 1)
+        {
+            Reverse(arr, 0, maxIndex);
+            result.Add(maxIndex + 1);
+            Reverse(arr, 0, n - 1);
+            result.Add(n);
+        }
+
+        PancakeSort(arr, n - 1, result);
+    }
+
+    private void Reverse(int[] arr, int start, int end)
+    {
+        while (start < end)
+        {
+            (arr[end], arr[start]) = (arr[start], arr[end]);
+            start++;
+            end--;
+        }
+    }
+}
