24b4ea408b5a19ff525d2bfdc78820d38edf68f4
Sukhrob Ilyosbekov <suxrobgm@gmail.com>
2025-05-17 16:23:43 -0400

    1450

---
+++ src/CSharp/Solutions/Solution1450.cs
@@ -0,0 +1,23 @@
+﻿using LeetCode.DataStructures;
+
+namespace LeetCode.Solutions;
+
+public class Solution1450
+{
+    /// <summary>
+    /// 1450. Number of Students Doing Homework at a Given Time - Easy
+    /// <a href="https://leetcode.com/problems/number-of-students-doing-homework-at-a-given-time">See the problem</a>
+    /// </summary>
+    public int BusyStudent(int[] startTime, int[] endTime, int queryTime)
+    {
+        int count = 0;
+        for (int i = 0; i < startTime.Length; i++)
+        {
+            if (startTime[i] <= queryTime && endTime[i] >= queryTime)
+            {
+                count++;
+            }
+        }
+        return count;
+    }
+}
