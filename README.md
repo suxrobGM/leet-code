ea41f4d214c4d90058966301b169a7feea186420
Sukhrob Ilyosbekov <suxrobgm@gmail.com>
2025-06-24 22:58:34 -0400

    1573

--- README.md
+++ README.md
@@ -1174,6 +1174,7 @@ My profile on LeetCode: https://leetcode.com/u/SuxrobGM
 | 1568 | [Minimum Number of Days to Disconnect Island](https://leetcode.com/problems/minimum-number-of-days-to-disconnect-island) | [C#](./src/CSharp/Solutions/Solution1568.cs) | Hard |
 | 1569 | [Number of Ways to Reorder Array to Get Same BST](https://leetcode.com/problems/number-of-ways-to-reorder-array-to-get-same-bst) | [C#](./src/CSharp/Solutions/Solution1569.cs) | Hard |
 | 1572 | [Matrix Diagonal Sum](https://leetcode.com/problems/matrix-diagonal-sum) | [C#](./src/CSharp/Solutions/Solution1572.cs) | Easy |
+| 1573 | [Number of Ways to Split a String](https://leetcode.com/problems/number-of-ways-to-split-a-string) | [C#](./src/CSharp/Solutions/Solution1573.cs) | Medium |
 | 1768 | [Merge Strings Alternately](https://leetcode.com/problems/merge-strings-alternately) | [C#](./src/CSharp/Solutions/Solution1768.cs) | Easy |
 | 1971 | [Find if Path Exists in Graph](https://leetcode.com/problems/find-if-path-exists-in-graph) | [C#](./src/CSharp/Solutions/Solution1971.cs) | Easy |
 | 2620 | [Counter](https://leetcode.com/problems/counter) | [TypeScript](./src/TypeScript/solutions/solution2620.ts) | Easy |
---
+++ src/CSharp/Solutions/Solution1573.cs
@@ -0,0 +1,45 @@
+﻿using System.Text;
+using LeetCode.DataStructures;
+
+namespace LeetCode.Solutions;
+
+public class Solution1573
+{
+    /// <summary>
+    /// 1573. Number of Ways to Split a String - Medium
+    /// <a href="https://leetcode.com/problems/number-of-ways-to-split-a-string">See the problem</a>
+    /// </summary>
+    public int NumWays(string s)
+    {
+        int MOD = 1_000_000_007;
+        int totalOnes = 0;
+        foreach (char c in s)
+        {
+            if (c == '1') totalOnes++;
+        }
+
+        if (totalOnes % 3 != 0) return 0;
+
+        int n = s.Length;
+
+        // Case when total number of 1s is 0
+        if (totalOnes == 0)
+        {
+            long ways = (long)(n - 1) * (n - 2) / 2;
+            return (int)(ways % MOD);
+        }
+
+        int onesPerPart = totalOnes / 3;
+        int firstSplitWays = 0, secondSplitWays = 0;
+        int onesCount = 0;
+
+        for (int i = 0; i < n; i++)
+        {
+            if (s[i] == '1') onesCount++;
+            if (onesCount == onesPerPart) firstSplitWays++;
+            else if (onesCount == 2 * onesPerPart) secondSplitWays++;
+        }
+
+        return (int)((long)firstSplitWays * secondSplitWays % MOD);
+    }
+}
