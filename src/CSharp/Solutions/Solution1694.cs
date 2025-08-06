2b9cdfb15b35a2d001664331bf0dfa72da3c7bf7
Sukhrob Ilyosbekov <suxrobgm@gmail.com>
2025-08-05 20:05:41 -0400

    1694

---
+++ src/CSharp/Solutions/Solution1694.cs
@@ -0,0 +1,41 @@
+﻿using System.Text;
+
+
+namespace LeetCode.Solutions;
+
+public class Solution1694
+{
+    /// <summary>
+    /// 1694. Reformat Phone Number - Easy
+    /// <a href="https://leetcode.com/problems/reformat-phone-number">See the problem</a>
+    /// </summary>
+    public string ReformatNumber(string number)
+    {
+        // Step 1: Remove all spaces and dashes
+        var digits = new string(number.Where(char.IsDigit).ToArray());
+        var result = new List<string>();
+        int i = 0;
+
+        while (digits.Length - i > 4)
+        {
+            result.Add(digits.Substring(i, 3));
+            i += 3;
+        }
+
+        int remaining = digits.Length - i;
+
+        if (remaining == 4)
+        {
+            result.Add(digits.Substring(i, 2));
+            result.Add(digits.Substring(i + 2, 2));
+        }
+        else
+        {
+            result.Add(digits.Substring(i, remaining));
+        }
+
+        return string.Join("-", result);
+    }
+}
+
+
