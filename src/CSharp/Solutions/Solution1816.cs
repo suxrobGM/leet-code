7acde01fc5688ee41763b8d6793a0191a79a1c39
Sukhrob Ilyosbekov <suxrobgm@gmail.com>
2025-09-09 21:24:01 -0400

    1816

---
+++ src/CSharp/Solutions/Solution1816.cs
@@ -0,0 +1,16 @@
+﻿using System.Text;
+
+namespace LeetCode.Solutions;
+
+public class Solution1816
+{
+    /// <summary>
+    /// 1816. Truncate Sentence - Easy
+    /// <a href="https://leetcode.com/problems/truncate-sentence">See the problem</a>
+    /// </summary>
+    public string TruncateSentence(string s, int k)
+    {
+        var words = s.Split(' ');
+        return string.Join(' ', words.Take(k));
+    }
+}
