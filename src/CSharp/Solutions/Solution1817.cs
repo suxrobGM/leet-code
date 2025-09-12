67b95e9134b5c92067346720f63be54b7ab82123
Sukhrob Ilyosbekov <suxrobgm@gmail.com>
2025-09-11 20:17:56 -0400

    1817

---
+++ src/CSharp/Solutions/Solution1817.cs
@@ -0,0 +1,39 @@
+﻿using System.Text;
+
+namespace LeetCode.Solutions;
+
+public class Solution1817
+{
+    /// <summary>
+    /// 1817. Finding the Users Active Minutes - Medium
+    /// <a href="https://leetcode.com/problems/finding-the-users-active-minutes">See the problem</a>
+    /// </summary>
+    public int[] FindingUsersActiveMinutes(int[][] logs, int k)
+    {
+        var userActivity = new Dictionary<int, HashSet<int>>();
+
+        foreach (var log in logs)
+        {
+            var userId = log[0];
+            var minute = log[1];
+
+            if (!userActivity.ContainsKey(userId))
+            {
+                userActivity[userId] = new HashSet<int>();
+            }
+            userActivity[userId].Add(minute);
+        }
+
+        var activeMinutes = new int[k];
+        foreach (var activity in userActivity.Values)
+        {
+            var count = activity.Count;
+            if (count > 0 && count <= k)
+            {
+                activeMinutes[count - 1]++;
+            }
+        }
+
+        return activeMinutes;
+    }
+}
