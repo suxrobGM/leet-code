6ab047cd15a5f0b664d32d5a9abcc5a05bdae4eb
Sukhrob Ilyosbekov <suxrobgm@gmail.com>
2025-09-22 17:17:16 -0400

    1835

---
+++ src/CSharp/Solutions/Solution1835.cs
@@ -0,0 +1,18 @@
+﻿using System.Text;
+
+namespace LeetCode.Solutions;
+
+public class Solution1835
+{
+    /// <summary>
+    /// 1835. Find XOR Sum of All Pairs Bitwise AND - Hard
+    /// <a href="https://leetcode.com/problems/find-xor-sum-of-all-pairs-bitwise-and">See the problem</a>
+    /// </summary>
+    public int GetXORSum(int[] arr1, int[] arr2)
+    {
+        int x1 = 0, x2 = 0;
+        foreach (var a in arr1) x1 ^= a;
+        foreach (var b in arr2) x2 ^= b;
+        return x1 & x2;
+    }
+}
