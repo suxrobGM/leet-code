92ce3c65f8a464e499a5618da43656e1c7bffd30
Sukhrob Ilyosbekov <suxrobgm@gmail.com>
2025-09-29 19:20:30 -0400

    1846

---
+++ src/CSharp/Solutions/Solution1846.cs
@@ -0,0 +1,24 @@
+﻿using System.Text;
+
+namespace LeetCode.Solutions;
+
+public class Solution1846
+{
+    /// <summary>
+    /// 1846. Maximum Element After Decreasing and Rearranging - Medium
+    /// <a href="https://leetcode.com/problems/maximum-element-after-decreasing-and-rearranging">See the problem</a>
+    /// </summary>
+    public int MaximumElementAfterDecrementingAndRearranging(int[] arr)
+    {
+        Array.Sort(arr);
+        arr[0] = 1;
+        for (int i = 1; i < arr.Length; i++)
+        {
+            if (arr[i] - arr[i - 1] > 1)
+            {
+                arr[i] = arr[i - 1] + 1;
+            }
+        }
+        return arr[^1];
+    }
+}
