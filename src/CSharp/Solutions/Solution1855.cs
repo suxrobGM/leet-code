83778374ca979accbd2dc206be287d8b37780300
Sukhrob Ilyosbekov <suxrobgm@gmail.com>
2025-10-06 15:48:14 -0400

    1855

---
+++ src/CSharp/Solutions/Solution1855.cs
@@ -0,0 +1,26 @@
+﻿using System.Numerics;
+using System.Text;
+
+namespace LeetCode.Solutions;
+
+public class Solution1855
+{
+    /// <summary>
+    /// 1855. Maximum Distance Between a Pair of Values - Medium
+    /// <a href="https://leetcode.com/problems/maximum-distance-between-a-pair-of-values">See the problem</a>
+    /// </summary>
+    public int MaxDistance(int[] nums1, int[] nums2)
+    {
+        int maxDistance = 0;
+        int j = 0;
+        for (int i = 0; i < nums1.Length; i++)
+        {
+            while (j < nums2.Length && nums1[i] <= nums2[j])
+            {
+                maxDistance = Math.Max(maxDistance, j - i);
+                j++;
+            }
+        }
+        return maxDistance;
+    }
+}
