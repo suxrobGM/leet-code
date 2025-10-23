1022724729ac42950b10b535b2b4a4ef0d6d6da3
Sukhrob Ilyosbekov <suxrobgm@gmail.com>
2025-10-23 18:11:56 -0400

    1877

---
+++ src/CSharp/Solutions/Solution1877.cs
@@ -0,0 +1,29 @@
+﻿using System.Numerics;
+using System.Text;
+
+namespace LeetCode.Solutions;
+
+public class Solution1877
+{
+    /// <summary>
+    /// 1877. Minimize Maximum Pair Sum in Array - Medium
+    /// <a href="https://leetcode.com/problems/minimize-maximum-pair-sum-in-array">See the problem</a>
+    /// </summary>
+    public int MinPairSum(int[] nums)
+    {
+        Array.Sort(nums);
+        int left = 0;
+        int right = nums.Length - 1;
+        int maxPairSum = 0;
+
+        while (left < right)
+        {
+            int pairSum = nums[left] + nums[right];
+            maxPairSum = Math.Max(maxPairSum, pairSum);
+            left++;
+            right--;
+        }
+
+        return maxPairSum;
+    }
+}
