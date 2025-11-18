0ce8d417885785bb9eb9faacc5ae5bc8f4650e27
Sukhrob Ilyosbekov <suxrobgm@gmail.com>
2025-11-18 16:15:31 -0500

    1907

---
+++ src/SQL/Solution1907.sql
@@ -0,0 +1,22 @@
+﻿-- 1907. Count Salary Categories - Medium
+-- Link: https://leetcode.com/problems/count-salary-categories
+-- Solution in PostgreSQL
+
+SELECT 'Low Salary' AS category,
+       COUNT(*) AS accounts_count
+FROM Accounts
+WHERE income < 20000
+
+UNION ALL
+
+SELECT 'Average Salary' AS category,
+       COUNT(*) AS accounts_count
+FROM Accounts
+WHERE income BETWEEN 20000 AND 50000
+
+UNION ALL
+
+SELECT 'High Salary' AS category,
+       COUNT(*) AS accounts_count
+FROM Accounts
+WHERE income > 50000;
