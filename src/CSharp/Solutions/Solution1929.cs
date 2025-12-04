5f4ec5ca69d17a1ed299f31c64bb2d467ef6c72e
Sukhrob Ilyosbekov <suxrobgm@gmail.com>
2025-12-03 22:17:46 -0500

    1929

---
+++ src/CSharp/Solutions/Solution1929.cs
@@ -0,0 +1,25 @@
+﻿using System.Numerics;
+using System.Text;
+
+namespace LeetCode.Solutions;
+
+public class Solution1929
+{
+    /// <summary>
+    /// 1929. Concatenation of Array - Easy
+    /// <a href="https://leetcode.com/problems/concatenation-of-array">See the problem</a>
+    /// </summary>
+    public int[] GetConcatenation(int[] nums)
+    {
+        int n = nums.Length;
+        int[] result = new int[2 * n];
+
+        for (int i = 0; i < n; i++)
+        {
+            result[i] = nums[i];
+            result[i + n] = nums[i];
+        }
+
+        return result;
+    }
+}
