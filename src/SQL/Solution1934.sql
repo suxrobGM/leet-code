af541d074055cec4e40ef2389272ed0f37e89f09
Sukhrob Ilyosbekov <suxrobgm@gmail.com>
2025-12-06 22:31:17 -0500

    1934

---
+++ src/SQL/Solution1934.sql
@@ -0,0 +1,18 @@
+﻿-- 1934. Confirmation Rate - Medium
+-- Link: https://leetcode.com/problems/confirmation-rate
+-- Solution in PostgreSQL
+
+SELECT 
+    s.user_id,
+    ROUND(
+        COALESCE(
+            SUM(CASE WHEN c.action = 'confirmed' THEN 1 ELSE 0 END)::DECIMAL / 
+            NULLIF(COUNT(c.time_stamp), 0),
+            0
+        ),
+        2
+    ) AS confirmation_rate
+FROM Signups s
+LEFT JOIN Confirmations c ON s.user_id = c.user_id
+GROUP BY s.user_id
+ORDER BY s.user_id;
