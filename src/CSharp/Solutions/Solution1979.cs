b9a262c5505544416db292f561ed061df3fb6b75
Sukhrob Ilyosbekov <suxrobgm@gmail.com>
2026-01-06 16:41:19 -0500

    1979

---
+++ src/CSharp/Solutions/Solution1979.cs
@@ -0,0 +1,33 @@
+﻿namespace LeetCode.Solutions;
+
+public class Solution1979
+{
+    /// <summary>
+    /// 1979. Find Greatest Common Divisor of Array - Easy
+    /// <a href="https://leetcode.com/problems/find-greatest-common-divisor-of-array">See the problem</a>
+    /// </summary>
+    public int FindGCD(int[] nums)
+    {
+        int min = int.MaxValue;
+        int max = int.MinValue;
+
+        foreach (var num in nums)
+        {
+            if (num < min) min = num;
+            if (num > max) max = num;
+        }
+
+        return GCD(min, max);
+    }
+
+    private int GCD(int a, int b)
+    {
+        while (b != 0)
+        {
+            int temp = b;
+            b = a % b;
+            a = temp;
+        }
+        return a;
+    }
+}
