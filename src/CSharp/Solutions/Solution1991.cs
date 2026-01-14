d18fd0a31fa19e11dba142b65706f69fb2f295fc
Sukhrob Ilyosbekov <suxrobgm@gmail.com>
2026-01-13 21:00:21 -0500

    1991

---
+++ src/CSharp/Solutions/Solution1991.cs
@@ -0,0 +1,30 @@
+﻿namespace LeetCode.Solutions;
+
+public class Solution1991
+{
+    /// <summary>
+    /// 1991. Find the Middle Index in Array - Easy
+    /// <a href="https://leetcode.com/problems/find-the-middle-index-in-array">See the problem</a>
+    /// </summary>
+    public int FindMiddleIndex(int[] nums)
+    {
+        int totalSum = 0;
+        foreach (var num in nums)
+        {
+            totalSum += num;
+        }
+
+        int leftSum = 0;
+        for (int i = 0; i < nums.Length; i++)
+        {
+            int rightSum = totalSum - leftSum - nums[i];
+            if (leftSum == rightSum)
+            {
+                return i;
+            }
+            leftSum += nums[i];
+        }
+
+        return -1;
+    }
+}
