c8fb5c948529fc3c2bc3796ae1517c5d4b97360a
Sukhrob Ilyosbekov <suxrobgm@gmail.com>
2026-01-19 20:06:04 -0500

    1997

---
+++ src/CSharp/Solutions/Solution1997.cs
@@ -0,0 +1,23 @@
+﻿namespace LeetCode.Solutions;
+
+public class Solution1997
+{
+    /// <summary>
+    /// 1997. First Day Where You Have Been in All the Rooms - Medium
+    /// <a href="https://leetcode.com/problems/first-day-where-you-have-been-in-all-the-rooms">See the problem</a>
+    /// </summary>
+    public int FirstDayBeenInAllRooms(int[] nextVisit)
+    {
+        const int MOD = 1_000_000_007;
+        int n = nextVisit.Length;
+        long[] dp = new long[n];
+        dp[0] = 0;
+
+        for (int i = 1; i < n; i++)
+        {
+            dp[i] = (2 * dp[i - 1] - dp[nextVisit[i - 1]] + 2 + MOD) % MOD;
+        }
+
+        return (int)dp[n - 1];
+    }
+}
