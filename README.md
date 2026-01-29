53e5270bbc0e1dbf7cc987d1d9dc7cc08b1e7261
Sukhrob Ilyosbekov <suxrobgm@gmail.com>
2026-01-28 20:03:37 -0500

    2008

--- README.md
+++ README.md
@@ -1508,6 +1508,7 @@ My profile on LeetCode: https://leetcode.com/u/SuxrobGM
 | 2003 | [Smallest Missing Genetic Value in Each Subtree](https://leetcode.com/problems/smallest-missing-genetic-value-in-each-subtree) | [C#](./src/CSharp/Solutions/Solution2003.cs) | Hard |
 | 2006 | [Count Number of Pairs With Absolute Difference K](https://leetcode.com/problems/count-number-of-pairs-with-absolute-difference-k) | [C#](./src/CSharp/Solutions/Solution2006.cs) | Easy |
 | 2007 | [Find Original Array From Doubled Array](https://leetcode.com/problems/find-original-array-from-doubled-array) | [C#](./src/CSharp/Solutions/Solution2007.cs) | Medium |
+| 2008 | [Maximum Earnings From Taxi](./src/CSharp/Solutions/Solution2008.cs) | Medium |
 | 2620 | [Counter](https://leetcode.com/problems/counter) | [TypeScript](./src/TypeScript/solutions/solution2620.ts) | Easy |
 | 2622 | [Cache With Time Limit](https://leetcode.com/problems/cache-with-time-limit) | [TypeScript](./src/TypeScript/solutions/solution2622.ts) | Medium |
 | 2621 | [Sleep](https://leetcode.com/problems/sleep) | [TypeScript](./src/TypeScript/solutions/solution2621.ts) | Easy |
---
+++ src/CSharp/Solutions/Solution2008.cs
@@ -0,0 +1,44 @@
+﻿namespace LeetCode.Solutions;
+
+public class Solution2008
+{
+    /// <summary>
+    /// 2008. Maximum Earnings From Taxi - Medium
+    /// <a href="https://leetcode.com/problems/maximum-earnings-from-taxi">See the problem</a>
+    /// </summary>
+    public long MaxTaxiEarnings(int n, int[][] rides)
+    {
+        // Group rides by their start point for efficient lookup
+        var ridesByStart = new Dictionary<int, List<int[]>>();
+        foreach (var ride in rides)
+        {
+            int start = ride[0];
+            if (!ridesByStart.ContainsKey(start))
+                ridesByStart[start] = new List<int[]>();
+            ridesByStart[start].Add(ride);
+        }
+
+        // dp[i] = maximum earnings starting from point i
+        long[] dp = new long[n + 2];
+
+        // Process from right to left
+        for (int i = n; i >= 1; i--)
+        {
+            // Option 1: Don't pick anyone at point i, move forward
+            dp[i] = dp[i + 1];
+
+            // Option 2: Pick up a passenger starting at point i
+            if (ridesByStart.TryGetValue(i, out var ridesAtI))
+            {
+                foreach (var ride in ridesAtI)
+                {
+                    int end = ride[1], tip = ride[2];
+                    long earnings = end - i + tip + dp[end];
+                    dp[i] = Math.Max(dp[i], earnings);
+                }
+            }
+        }
+
+        return dp[1];
+    }
+}
