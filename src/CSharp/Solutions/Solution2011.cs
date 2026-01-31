be6848271624088e5b9152e874623a0300d52b39
Sukhrob Ilyosbekov <suxrobgm@gmail.com>
2026-01-30 19:45:25 -0500

    2011

---
+++ src/CSharp/Solutions/Solution2011.cs
@@ -0,0 +1,18 @@
+﻿namespace LeetCode.Solutions;
+
+public class Solution2011
+{
+    /// <summary>
+    /// 2011. Final Value of Variable After Performing Operations - Easy
+    /// <a href="https://leetcode.com/problems/final-value-of-variable-after-performing-operations">See the problem</a>
+    /// </summary>
+    public int FinalValueAfterOperations(string[] operations)
+    {
+        int x = 0;
+        foreach (var op in operations)
+        {
+            x += op.Contains('+') ? 1 : -1;
+        }
+        return x;
+    }
+}
