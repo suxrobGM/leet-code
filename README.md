75562b3f7f44135b5cf33a2f2682fb3873275525
Sukhrob Ilyosbekov <suxrobgm@gmail.com>
2026-02-06 20:34:12 -0500

    2019

--- README.md
+++ README.md
@@ -1518,6 +1518,7 @@ My profile on LeetCode: <https://leetcode.com/u/SuxrobGM>
 | 2016 | [Maximum Difference Between Increasing Elements](https://leetcode.com/problems/maximum-difference-between-increasing-elements) | [TypeScript](./src/TypeScript/solutions/solution2016.ts) | Easy |
 | 2017 | [Grid Game](https://leetcode.com/problems/grid-game) | [TypeScript](./src/TypeScript/solutions/solution2017.ts) | Medium |
 | 2018 | [Check if Word Can Be Placed In Crossword](https://leetcode.com/problems/check-if-word-can-be-placed-in-crossword) | [TypeScript](./src/TypeScript/solutions/solution2018.ts) | Medium |
+| 2019 | [The Score of Students Solving Math Expression](https://leetcode.com/problems/the-score-of-students-solving-math-expression) | [TypeScript](./src/TypeScript/solutions/solution2019.ts) | Hard |
 | 2620 | [Counter](https://leetcode.com/problems/counter) | [TypeScript](./src/TypeScript/solutions/solution2620.ts) | Easy |
 | 2622 | [Cache With Time Limit](https://leetcode.com/problems/cache-with-time-limit) | [TypeScript](./src/TypeScript/solutions/solution2622.ts) | Medium |
 | 2621 | [Sleep](https://leetcode.com/problems/sleep) | [TypeScript](./src/TypeScript/solutions/solution2621.ts) | Easy |
---
+++ src/CSharp/Solutions/Solution2019.cs
@@ -0,0 +1,118 @@
+﻿namespace LeetCode.Solutions;
+
+public class Solution2019
+{
+    /// <summary>
+    /// 2019. The Score of Students Solving Math Expression - Hard
+    /// <a href="https://leetcode.com/problems/the-score-of-students-solving-math-expression">See the problem</a>
+    /// </summary>
+    public int ScoreOfStudents(string s, int[] answers)
+    {
+        var nums = new List<int>();
+        var ops = new List<char>();
+
+        for (var i = 0; i < s.Length; i++)
+        {
+            var c = s[i];
+            if (c == '+' || c == '*')
+            {
+                ops.Add(c);
+            }
+            else
+            {
+                nums.Add(c - '0');
+            }
+        }
+
+        var correct = EvaluateCorrect(nums, ops);
+        var possible = ComputeAllResults(nums, ops);
+
+        var score = 0;
+        foreach (var ans in answers)
+        {
+            if (ans == correct)
+            {
+                score += 5;
+            }
+            else if (ans <= 1000 && possible.Contains(ans))
+            {
+                score += 2;
+            }
+        }
+
+        return score;
+    }
+
+    private static int EvaluateCorrect(List<int> nums, List<char> ops)
+    {
+        var stack = new List<int> { nums[0] };
+
+        for (var i = 0; i < ops.Count; i++)
+        {
+            var op = ops[i];
+            var value = nums[i + 1];
+
+            if (op == '*')
+            {
+                stack[stack.Count - 1] *= value;
+            }
+            else
+            {
+                stack.Add(value);
+            }
+        }
+
+        var sum = 0;
+        foreach (var v in stack)
+        {
+            sum += v;
+        }
+
+        return sum;
+    }
+
+    private static HashSet<int> ComputeAllResults(List<int> nums, List<char> ops)
+    {
+        var memo = new Dictionary<(int Left, int Right), HashSet<int>>();
+        return Dfs(0, nums.Count - 1);
+
+        HashSet<int> Dfs(int left, int right)
+        {
+            if (memo.TryGetValue((left, right), out var cached))
+            {
+                return cached;
+            }
+
+            var result = new HashSet<int>();
+
+            if (left == right)
+            {
+                result.Add(nums[left]);
+                memo[(left, right)] = result;
+                return result;
+            }
+
+            for (var k = left; k < right; k++)
+            {
+                var op = ops[k];
+                var leftSet = Dfs(left, k);
+                var rightSet = Dfs(k + 1, right);
+
+                foreach (var a in leftSet)
+                {
+                    foreach (var b in rightSet)
+                    {
+                        var value = op == '+' ? a + b : a * b;
+                        if (value <= 1000)
+                        {
+                            result.Add(value);
+                        }
+                    }
+                }
+            }
+
+            memo[(left, right)] = result;
+            return result;
+        }
+    }
+}
