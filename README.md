7b069dfa8bf4a95d9abab2ea92cf6f705a2a8edd
Sukhrob Ilyosbekov <suxrobgm@gmail.com>
2026-02-09 21:45:00 -0500

    2024

--- README.md
+++ README.md
@@ -1521,6 +1521,7 @@ My profile on LeetCode: <https://leetcode.com/u/SuxrobGM>
 | 2019 | [The Score of Students Solving Math Expression](https://leetcode.com/problems/the-score-of-students-solving-math-expression) | [TypeScript](./src/TypeScript/solutions/solution2019.ts) | Hard |
 | 2022 | [Convert 1D Array Into 2D Array](https://leetcode.com/problems/convert-1d-array-into-2d-array) | [TypeScript](./src/TypeScript/solutions/solution2022.ts) | Easy |
 | 2023 | [Number of Pairs of Strings With Concatenation Equal to Target](https://leetcode.com/problems/number-of-pairs-of-strings-with-concatenation-equal-to-target) | [TypeScript](./src/TypeScript/solutions/solution2023.ts) | Medium |
+| 2024 | [Maximize the Confusion of an Exam](https://leetcode.com/problems/maximize-the-confusion-of-an-exam) | [TypeScript](./src/TypeScript/solutions/solution2024.ts) | Medium |
 | 2620 | [Counter](https://leetcode.com/problems/counter) | [TypeScript](./src/TypeScript/solutions/solution2620.ts) | Easy |
 | 2622 | [Cache With Time Limit](https://leetcode.com/problems/cache-with-time-limit) | [TypeScript](./src/TypeScript/solutions/solution2622.ts) | Medium |
 | 2621 | [Sleep](https://leetcode.com/problems/sleep) | [TypeScript](./src/TypeScript/solutions/solution2621.ts) | Easy |
---
+++ src/CSharp/Solutions/Solution2024.cs
@@ -0,0 +1,48 @@
+﻿namespace LeetCode.Solutions;
+
+public class Solution2024
+{
+    /// <summary>
+    /// 2024. Maximize the Confusion of an Exam - Medium
+    /// <a href="https://leetcode.com/problems/maximize-the-confusion-of-an-exam">See the problem</a>
+    /// </summary>
+    public int MaxConsecutiveAnswers(string answerKey, int k)
+    {
+        var maxConsecutiveT = 0;
+        var maxConsecutiveF = 0;
+        var countT = 0;
+        var countF = 0;
+        var left = 0;
+
+        for (var right = 0; right < answerKey.Length; right++)
+        {
+            if (answerKey[right] == 'T')
+            {
+                countT++;
+            }
+            else
+            {
+                countF++;
+            }
+
+            while (countT > k && countF > k)
+            {
+                if (answerKey[left] == 'T')
+                {
+                    countT--;
+                }
+                else
+                {
+                    countF--;
+                }
+
+                left++;
+            }
+
+            maxConsecutiveT = Math.Max(maxConsecutiveT, right - left + 1);
+            maxConsecutiveF = Math.Max(maxConsecutiveF, right - left + 1);
+        }
+
+        return Math.Max(maxConsecutiveT, maxConsecutiveF);
+    }
+}
