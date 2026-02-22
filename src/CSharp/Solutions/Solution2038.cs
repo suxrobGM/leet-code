b3d49005cb853c2455559a81699c5b4576f3f41e
Sukhrob Ilyosbekov <suxrobgm@gmail.com>
2026-02-21 23:20:14 -0500

    2038

---
+++ src/CSharp/Solutions/Solution2038.cs
@@ -0,0 +1,24 @@
+﻿namespace LeetCode.Solutions;
+
+public class Solution2038
+{
+    /// <summary>
+    /// 2038. Remove Colored Pieces if Both Neighbors are the Same Color - Medium
+    /// <a href="https://leetcode.com/problems/remove-colored-pieces-if-both-neighbors-are-the-same-color">See the problem</a>
+    /// </summary>
+    public bool WinnerOfGame(string colors)
+    {
+        var alice = 0;
+        var bob = 0;
+
+        for (var i = 1; i < colors.Length - 1; i++)
+        {
+            if (colors[i] == 'A' && colors[i - 1] == 'A' && colors[i + 1] == 'A')
+                alice++;
+            else if (colors[i] == 'B' && colors[i - 1] == 'B' && colors[i + 1] == 'B')
+                bob++;
+        }
+
+        return alice > bob;
+    }
+}
