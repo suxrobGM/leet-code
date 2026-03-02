878c2cecffe9031c784e2cd9c8ecafe1ea2a85f4
Sukhrob Ilyosbekov <suxrobgm@gmail.com>
2026-03-01 20:05:01 -0500

    2048

---
+++ src/CSharp/Solutions/Solution2048.cs
@@ -0,0 +1,36 @@
+﻿namespace LeetCode.Solutions;
+
+public class Solution2048
+{
+    /// <summary>
+    /// 2048. Next Greater Numerically Balanced Number - Medium
+    /// <a href="https://leetcode.com/problems/next-greater-numerically-balanced-number">See the problem</a>
+    /// </summary>
+    public int NextBeautifulNumber(int n)
+    {
+        while (true)
+        {
+            n++;
+            if (IsBalanced(n))
+                return n;
+        }
+    }
+
+    private static bool IsBalanced(int n)
+    {
+        Span<int> count = stackalloc int[10];
+        while (n > 0)
+        {
+            count[n % 10]++;
+            n /= 10;
+        }
+
+        for (var d = 0; d < 10; d++)
+        {
+            if (count[d] != 0 && count[d] != d)
+                return false;
+        }
+
+        return true;
+    }
+}
