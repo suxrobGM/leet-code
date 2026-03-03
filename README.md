08b0b625a11271c839480012c47ae5df778c9b62
Sukhrob Ilyosbekov <suxrobgm@gmail.com>
2026-03-02 19:33:24 -0500

    2049

--- README.md
+++ README.md
@@ -1541,6 +1541,7 @@ My profile on LeetCode: <https://leetcode.com/u/SuxrobGM>
 | 2045 | [Second Minimum Time to Reach Destination](https://leetcode.com/problems/second-minimum-time-to-reach-destination) | [TypeScript](./src/TypeScript/solutions/solution2045.ts) | Hard |
 | 2047 | [Number of Valid Words in a Sentence](https://leetcode.com/problems/number-of-valid-words-in-a-sentence) | [TypeScript](./src/TypeScript/solutions/solution2047.ts) | Easy |
 | 2048 | [Next Greater Numerically Balanced Number](https://leetcode.com/problems/next-greater-numerically-balanced-number) | [TypeScript](./src/TypeScript/solutions/solution2048.ts) | Medium |
+| 2049 | [Count Nodes With the Highest Score](https://leetcode.com/problems/count-nodes-with-the-highest-score) | [TypeScript](./src/TypeScript/solutions/solution2049.ts) | Medium |
 | 2620 | [Counter](https://leetcode.com/problems/counter) | [TypeScript](./src/TypeScript/solutions/solution2620.ts) | Easy |
 | 2622 | [Cache With Time Limit](https://leetcode.com/problems/cache-with-time-limit) | [TypeScript](./src/TypeScript/solutions/solution2622.ts) | Medium |
 | 2621 | [Sleep](https://leetcode.com/problems/sleep) | [TypeScript](./src/TypeScript/solutions/solution2621.ts) | Easy |
---
+++ src/CSharp/Solutions/Solution2049.cs
@@ -0,0 +1,49 @@
+﻿namespace LeetCode.Solutions;
+
+public class Solution2049
+{
+    /// <summary>
+    /// 2049. Count Nodes With the Highest Score - Medium
+    /// <a href="https://leetcode.com/problems/count-nodes-with-the-highest-score">See the problem</a>
+    /// </summary>
+    public int CountHighestScoreNodes(int[] parents)
+    {
+        var n = parents.Length;
+        var children = new List<int>[n];
+        for (var i = 0; i < n; i++)
+            children[i] = new List<int>();
+
+        for (var i = 1; i < n; i++)
+            children[parents[i]].Add(i);
+
+        var maxScore = 0L;
+        var count = 0;
+        Dfs(0);
+        return count;
+
+        long Dfs(int node)
+        {
+            var score = 1L;
+            var size = 1L;
+            foreach (var child in children[node])
+            {
+                var childSize = Dfs(child);
+                score *= childSize;
+                size += childSize;
+            }
+
+            if (node != 0)
+                score *= Math.Max(1, n - size);
+
+            if (score > maxScore)
+            {
+                maxScore = score;
+                count = 1;
+            }
+            else if (score == maxScore)
+                count++;
+
+            return size;
+        }
+    }
+}
