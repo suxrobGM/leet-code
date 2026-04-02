3449393eb25703c7e67cb7a66f29bb87cfd2b1bf
Sukhrob Ilyosbekov <suxrobgm@gmail.com>
2026-04-01 20:46:40 -0400

    2088

---
+++ src/CSharp/Solutions/Solution2088.cs
@@ -0,0 +1,42 @@
+﻿namespace LeetCode.Solutions;
+
+public class Solution2088
+{
+    /// <summary>
+    /// 2088. Count Fertile Pyramids in a Land - Hard
+    /// <a href="https://leetcode.com/problems/count-fertile-pyramids-in-a-land">See the problem</a>
+    /// </summary>
+    public int CountPyramids(int[][] grid)
+    {
+        var m = grid.Length;
+        var n = grid[0].Length;
+        var result = 0;
+
+        // Count inverse pyramids (apex at bottom)
+        var dp = new int[m][];
+        for (var i = 0; i < m; i++)
+            dp[i] = (int[])grid[i].Clone();
+
+        for (var i = 1; i < m; i++)
+            for (var j = 1; j < n - 1; j++)
+                if (dp[i][j] == 1)
+                {
+                    dp[i][j] = Math.Min(dp[i - 1][j - 1], Math.Min(dp[i - 1][j], dp[i - 1][j + 1])) + 1;
+                    result += dp[i][j] - 1;
+                }
+
+        // Count regular pyramids (apex at top)
+        for (var i = 0; i < m; i++)
+            dp[i] = (int[])grid[i].Clone();
+
+        for (var i = m - 2; i >= 0; i--)
+            for (var j = 1; j < n - 1; j++)
+                if (dp[i][j] == 1)
+                {
+                    dp[i][j] = Math.Min(dp[i + 1][j - 1], Math.Min(dp[i + 1][j], dp[i + 1][j + 1])) + 1;
+                    result += dp[i][j] - 1;
+                }
+
+        return result;
+    }
+}
