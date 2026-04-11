3aa610f80e56b2f3f65687b02f2fe41d7cdb620b
Sukhrob Ilyosbekov <suxrobgm@gmail.com>
2026-04-11 18:08:48 -0400

    2097

--- README.md
+++ README.md
@@ -1579,6 +1579,7 @@ My profile on LeetCode: <https://leetcode.com/u/SuxrobGM>
 | 2094 | [Finding 3-Digit Even Numbers](https://leetcode.com/problems/finding-3-digit-even-numbers) | [TypeScript](./src/TypeScript/solutions/solution2094.ts) | Easy |
 | 2095 | [Delete the Middle Node of a Linked List](https://leetcode.com/problems/delete-the-middle-node-of-a-linked-list) | [TypeScript](./src/TypeScript/solutions/solution2095.ts) | Medium |
 | 2096 | [Step-By-Step Directions From a Binary Tree Node to Another](https://leetcode.com/problems/step-by-step-directions-from-a-binary-tree-node-to-another) | [TypeScript](./src/TypeScript/solutions/solution2096.ts) | Medium |
+| 2097 | [Valid Arrangement of Pairs](https://leetcode.com/problems/valid-arrangement-of-pairs) | [TypeScript](./src/TypeScript/solutions/solution2097.ts) | Hard |
 | 2622 | [Cache With Time Limit](https://leetcode.com/problems/cache-with-time-limit) | [TypeScript](./src/TypeScript/solutions/solution2622.ts) | Medium |
 | 2621 | [Sleep](https://leetcode.com/problems/sleep) | [TypeScript](./src/TypeScript/solutions/solution2621.ts) | Easy |
 | 2623 | [Memoize](https://leetcode.com/problems/memoize) | [TypeScript](./src/TypeScript/solutions/solution2623.ts) | Medium |
---
+++ src/CSharp/Solutions/Solution2097.cs
@@ -0,0 +1,55 @@
+﻿using System.Text;
+using LeetCode.DataStructures;
+
+namespace LeetCode.Solutions;
+
+public class Solution2097
+{
+    /// <summary>
+    /// 2097. Valid Arrangement of Pairs - Hard
+    /// <a href="https://leetcode.com/problems/valid-arrangement-of-pairs">See the problem</a>
+    /// </summary>
+    public int[][] ValidArrangement(int[][] pairs)
+    {
+        var graph = new Dictionary<int, Stack<int>>();
+        var degree = new Dictionary<int, int>(); // out - in
+
+        foreach (var pair in pairs)
+        {
+            int start = pair[0], end = pair[1];
+            if (!graph.ContainsKey(start)) graph[start] = new Stack<int>();
+            graph[start].Push(end);
+
+            degree[start] = degree.GetValueOrDefault(start) + 1;
+            degree[end] = degree.GetValueOrDefault(end) - 1;
+        }
+
+        // Find start node: node with out - in == 1, or any node if Eulerian circuit
+        var startNode = pairs[0][0];
+        foreach (var (node, deg) in degree)
+            if (deg == 1) { startNode = node; break; }
+
+        var path = new Stack<int>();
+        var stack = new Stack<int>();
+        stack.Push(startNode);
+
+        while (stack.Count > 0)
+        {
+            var node = stack.Peek();
+            if (graph.ContainsKey(node) && graph[node].Count > 0)
+                stack.Push(graph[node].Pop());
+            else
+            {
+                path.Push(node);
+                stack.Pop();
+            }
+        }
+
+        var nodes = path.ToArray();
+        var result = new int[nodes.Length - 1][];
+        for (var i = 0; i < result.Length; i++)
+            result[i] = [nodes[i], nodes[i + 1]];
+
+        return result;
+    }
+}
