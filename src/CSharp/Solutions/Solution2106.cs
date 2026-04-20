5e22d0d88b3a9fbdfbf4f2536867b846a1d19aa7
Sukhrob Ilyosbekov <suxrobgm@gmail.com>
2026-04-19 22:50:47 -0400

    2106

---
+++ src/CSharp/Solutions/Solution2106.cs
@@ -0,0 +1,42 @@
+﻿using System.Text;
+using LeetCode.DataStructures;
+
+namespace LeetCode.Solutions;
+
+public class Solution2106
+{
+    /// <summary>
+    /// 2106. Maximum Fruits Harvested After at Most K Steps - Hard
+    /// <a href="https://leetcode.com/problems/maximum-fruits-harvested-after-at-most-k-steps">See the problem</a>
+    /// </summary>
+    public int MaxTotalFruits(int[][] fruits, int startPos, int k)
+    {
+        var n = fruits.Length;
+        var best = 0;
+        var sum = 0;
+        var i = 0;
+
+        for (var j = 0; j < n; j++)
+        {
+            sum += fruits[j][1];
+
+            while (i <= j && Cost(fruits[i][0], fruits[j][0], startPos) > k)
+            {
+                sum -= fruits[i][1];
+                i++;
+            }
+
+            if (i <= j)
+            {
+                best = Math.Max(best, sum);
+            }
+        }
+
+        return best;
+    }
+
+    private static int Cost(int left, int right, int start)
+    {
+        return right - left + Math.Min(Math.Abs(start - left), Math.Abs(start - right));
+    }
+}
