79a7988420aedc35948c2215475e3f1df72c8ac7
Sukhrob Ilyosbekov <suxrobgm@gmail.com>
2026-05-05 23:34:15 -0400

    2126

---
+++ src/CSharp/Solutions/Solution2126.cs
@@ -0,0 +1,24 @@
+﻿namespace LeetCode.Solutions;
+
+public class Solution2126
+{
+    /// <summary>
+    /// 2126. Destroying Asteroids - Medium
+    /// <a href="https://leetcode.com/problems/destroying-asteroids">See the problem</a>
+    /// </summary>
+    public bool AsteroidsDestroyed(int mass, int[] asteroids)
+    {
+        Array.Sort(asteroids);
+        long current = mass;
+        foreach (var asteroid in asteroids)
+        {
+            if (current < asteroid)
+            {
+                return false;
+            }
+            current += asteroid;
+        }
+
+        return true;
+    }
+}
