f5915206e54bc18f34f2a28684a208591daf502a
Sukhrob Ilyosbekov <suxrobgm@gmail.com>
2026-05-20 23:05:49 -0400

    2144

---
+++ src/CSharp/Solutions/Solution2144.cs
@@ -0,0 +1,20 @@
+﻿namespace LeetCode.Solutions;
+
+public class Solution2144
+{
+    /// <summary>
+    /// 2144. Minimum Cost of Buying Candies With Discount - Easy
+    /// <a href="https://leetcode.com/problems/minimum-cost-of-buying-candies-with-discount">See the problem</a>
+    /// </summary>
+    public int MinimumCost(int[] cost)
+    {
+        Array.Sort(cost);
+        int total = 0;
+        for (int i = cost.Length - 1; i >= 0; i -= 3)
+        {
+            total += cost[i];
+            if (i - 1 >= 0) total += cost[i - 1];
+        }
+        return total;
+    }
+}
