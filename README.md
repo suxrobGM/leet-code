52620f232f299f75cc41fbb6232b8b14a4cd335b
Sukhrob Ilyosbekov <suxrobgm@gmail.com>
2026-06-09 21:09:40 -0400

    2166

--- README.md
+++ README.md
@@ -1634,6 +1634,7 @@ My profile on LeetCode: <https://leetcode.com/u/SuxrobGM>
 | 2163 | [Minimum Difference in Sums After Removal of Elements](https://leetcode.com/problems/minimum-difference-in-sums-after-removal-of-elements) | [TypeScript](./src/TypeScript/solutions/solution2163.ts) | Hard |
 | 2164 | [Sort Even and Odd Indices Independently](https://leetcode.com/problems/sort-even-and-odd-indices-independently) | [TypeScript](./src/TypeScript/solutions/solution2164.ts) | Easy |
 | 2165 | [Smallest Value of the Rearranged Number](https://leetcode.com/problems/smallest-value-of-the-rearranged-number) | [TypeScript](./src/TypeScript/solutions/solution2165.ts) | Medium |
+| 2166 | [Minimum Cost to Make Array Equal](https://leetcode.com/problems/minimum-cost-to-make-array-equal) | [TypeScript](./src/TypeScript/solutions/solution2166.ts) | Medium |
 | 2622 | [Cache With Time Limit](https://leetcode.com/problems/cache-with-time-limit) | [TypeScript](./src/TypeScript/solutions/solution2622.ts) | Medium |
 | 2621 | [Sleep](https://leetcode.com/problems/sleep) | [TypeScript](./src/TypeScript/solutions/solution2621.ts) | Easy |
 | 2623 | [Memoize](https://leetcode.com/problems/memoize) | [TypeScript](./src/TypeScript/solutions/solution2623.ts) | Medium |
---
+++ src/CSharp/Solutions/Solution2166.cs
@@ -0,0 +1,64 @@
+﻿namespace LeetCode.Solutions;
+
+public class Solution2166
+{
+    /// <summary>
+    /// 2166. Design Bitset - Medium
+    /// <a href="https://leetcode.com/problems/design-bitset">See the problem</a>
+    /// </summary>
+    public class Bitset
+    {
+        private readonly bool[] _bits;
+        private readonly int _size;
+        private bool _flipped;   // when true, every stored bit is logically inverted
+        private int _ones;       // number of logical 1s currently set
+
+        public Bitset(int size)
+        {
+            _size = size;
+            _bits = new bool[size];
+            _flipped = false;
+            _ones = 0;
+        }
+
+        public void Fix(int idx)
+        {
+            // Logical value at idx is _bits[idx] ^ _flipped; we want it to be 1.
+            if ((_bits[idx] ^ _flipped) == false)
+            {
+                _bits[idx] = !_bits[idx];
+                _ones++;
+            }
+        }
+
+        public void Unfix(int idx)
+        {
+            // We want the logical value at idx to be 0.
+            if ((_bits[idx] ^ _flipped) == true)
+            {
+                _bits[idx] = !_bits[idx];
+                _ones--;
+            }
+        }
+
+        public void Flip()
+        {
+            _flipped = !_flipped;
+            _ones = _size - _ones;
+        }
+
+        public bool All() => _ones == _size;
+
+        public bool One() => _ones > 0;
+
+        public int Count() => _ones;
+
+        public override string ToString()
+        {
+            var sb = new System.Text.StringBuilder(_size);
+            for (int i = 0; i < _size; i++)
+                sb.Append(_bits[i] ^ _flipped ? '1' : '0');
+            return sb.ToString();
+        }
+    }
+}
