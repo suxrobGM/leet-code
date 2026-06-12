6e04021723f1fecf4c46861c83824048e25960e4
Sukhrob Ilyosbekov <suxrobgm@gmail.com>
2026-06-11 21:38:05 -0400

    2169

---
+++ src/CSharp/Solutions/Solution2169.cs
@@ -0,0 +1,26 @@
+﻿namespace LeetCode.Solutions;
+
+public class Solution2169
+{
+    /// <summary>
+    /// 2169. Count Operations to Obtain Zero - Easy
+    /// <a href="https://leetcode.com/problems/count-operations-to-obtain-zero">See the problem</a>
+    /// </summary>
+    public int CountOperations(int num1, int num2)
+    {
+        var count = 0;
+        while (num1 > 0 && num2 > 0)
+        {
+            if (num1 > num2)
+            {
+                num1 -= num2;
+            }
+            else
+            {
+                num2 -= num1;
+            }
+            count++;
+        }
+        return count;
+    }
+}
