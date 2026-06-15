14f40f6778c452a97d1ac52073de6a2a104a3cdd
Sukhrob Ilyosbekov <suxrobgm@gmail.com>
2026-06-14 20:14:37 -0400

    2171

---
+++ src/CSharp/Solutions/Solution2171.cs
@@ -0,0 +1,22 @@
+﻿namespace LeetCode.Solutions;
+
+public class Solution2171
+{
+    /// <summary>
+    /// 2171. Removing Minimum Number of Magic Beans - Medium
+    /// <a href="https://leetcode.com/problems/removing-minimum-number-of-magic-beans">See the problem</a>
+    /// </summary>
+    public long MinimumRemoval(int[] beans)
+    {
+        Array.Sort(beans);
+        long total = beans.Sum(x => (long)x);
+        long max = 0;
+        long current = 0;
+        for (int i = 0; i < beans.Length; i++)
+        {
+            current += beans[i];
+            max = Math.Max(max, (long)beans[i] * (beans.Length - i));
+        }
+        return total - max;
+    }
+}
