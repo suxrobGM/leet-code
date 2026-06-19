5263c7013841dd6c8140bb4e31f8d19180f8f3a8
Sukhrob Ilyosbekov <suxrobgm@gmail.com>
2026-06-18 22:31:05 -0400

    2177

---
+++ src/CSharp/Solutions/Solution2177.cs
@@ -0,0 +1,19 @@
+﻿namespace LeetCode.Solutions;
+
+public class Solution2177
+{
+    /// <summary>
+    /// 2177. Find Three Consecutive Integers That Sum to a Given Number - Medium
+    /// <a href="https://leetcode.com/problems/find-three-consecutive-integers-that-sum-to-a-given-number">See the problem</a>
+    /// </summary>
+    public long[] SumOfThree(long num)
+    {
+        if (num % 3 != 0)
+        {
+            return new long[0];
+        }
+
+        long middle = num / 3;
+        return new long[] { middle - 1, middle, middle + 1 };
+    }
+}
