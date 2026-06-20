0775c0be2d2ba8f963d687a7b3e30cc6aca1b014
Sukhrob Ilyosbekov <suxrobgm@gmail.com>
2026-06-19 21:43:47 -0400

    2178

---
+++ src/CSharp/Solutions/Solution2178.cs
@@ -0,0 +1,35 @@
+﻿namespace LeetCode.Solutions;
+
+public class Solution2178
+{
+    /// <summary>
+    /// 2178. Maximum Split of Positive Even Integers - Medium
+    /// <a href="https://leetcode.com/problems/maximum-split-of-positive-even-integers">See the problem</a>
+    /// </summary>
+    public IList<long> MaximumEvenSplit(long finalSum)
+    {
+        if (finalSum % 2 != 0)
+        {
+            return new List<long>();
+        }
+
+        var result = new List<long>();
+        long current = 2;
+        while (finalSum > 0)
+        {
+            if (finalSum - current > current)
+            {
+                result.Add(current);
+                finalSum -= current;
+                current += 2;
+            }
+            else
+            {
+                result.Add(finalSum);
+                break;
+            }
+        }
+
+        return result;
+    }
+}
