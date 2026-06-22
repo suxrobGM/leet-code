1027351e18f2db01dff82f3262e799afaec38ad4
Sukhrob Ilyosbekov <suxrobgm@gmail.com>
2026-06-21 21:12:58 -0400

    2180

---
+++ src/CSharp/Solutions/Solution2180.cs
@@ -0,0 +1,28 @@
+﻿namespace LeetCode.Solutions;
+
+public class Solution2180
+{
+    /// <summary>
+    /// 2180. Count Integers With Even Digit Sum - Easy
+    /// <a href="https://leetcode.com/problems/count-integers-with-even-digit-sum">See the problem</a>
+    /// </summary>
+    public int CountEven(int num)
+    {
+        var count = 0;
+        for (var i = 1; i <= num; i++)
+        {
+            var sum = 0;
+            for (var n = i; n > 0; n /= 10)
+            {
+                sum += n % 10;
+            }
+
+            if (sum % 2 == 0)
+            {
+                count++;
+            }
+        }
+
+        return count;
+    }
+}
