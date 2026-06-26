8bd889b5a0bdf481148a2a50ce952a29316fefc7
Sukhrob Ilyosbekov <suxrobgm@gmail.com>
2026-06-26 18:33:02 -0400

    2185

---
+++ src/CSharp/Solutions/Solution2185.cs
@@ -0,0 +1,23 @@
+﻿namespace LeetCode.Solutions;
+
+public class Solution2185
+{
+    /// <summary>
+    /// 2185. Counting Words With a Given Prefix - Easy
+    /// <a href="https://leetcode.com/problems/counting-words-with-a-given-prefix">See the problem</a>
+    /// </summary>
+    public int PrefixCount(string[] words, string pref)
+    {
+        var count = 0;
+
+        foreach (var word in words)
+        {
+            if (word.StartsWith(pref))
+            {
+                count++;
+            }
+        }
+
+        return count;
+    }
+}
