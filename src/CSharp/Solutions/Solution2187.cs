c80e12425084641b2ea53c63f8b14b7ef6ad5e22
Sukhrob Ilyosbekov <suxrobgm@gmail.com>
2026-06-28 17:22:29 -0400

    2187

---
+++ src/CSharp/Solutions/Solution2187.cs
@@ -0,0 +1,36 @@
+﻿namespace LeetCode.Solutions;
+
+public class Solution2187
+{
+    /// <summary>
+    /// 2186. Minimum Time to Complete Trips - Medium
+    /// <a href="https://leetcode.com/problems/minimum-time-to-complete-trips">See the problem</a>
+    /// </summary>
+    public long MinimumTime(int[] time, int totalTrips)
+    {
+        long left = 1;
+        long right = (long)totalTrips * time.Max();
+
+        while (left < right)
+        {
+            long mid = left + (right - left) / 2;
+            long trips = 0;
+
+            foreach (var t in time)
+            {
+                trips += mid / t;
+            }
+
+            if (trips >= totalTrips)
+            {
+                right = mid;
+            }
+            else
+            {
+                left = mid + 1;
+            }
+        }
+
+        return left;
+    }
+}
