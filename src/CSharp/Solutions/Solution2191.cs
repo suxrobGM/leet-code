7659491e1be3bede0d246dca51808d8369cee227
Sukhrob Ilyosbekov <suxrobgm@gmail.com>
2026-07-01 20:41:35 -0400

    2191

---
+++ src/CSharp/Solutions/Solution2191.cs
@@ -0,0 +1,13 @@
+﻿namespace LeetCode.Solutions;
+
+public class Solution2191
+{
+    /// <summary>
+    /// 2191. Sort the Jumbled Numbers - Medium
+    /// <a href="https://leetcode.com/problems/sort-the-jumbled-numbers">See the problem</a>
+    /// </summary>
+    public int[] SortJumbled(int[] mapping, int[] nums)
+    {
+        return nums.OrderBy(x => x.ToString().Select(c => mapping[c - '0']).Aggregate((a, b) => a * 10 + b)).ToArray();
+    }
+}
