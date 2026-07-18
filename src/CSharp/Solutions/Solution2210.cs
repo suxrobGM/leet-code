11d5daeff9a9c7a5756a43c370a4142343b764f5
Sukhrob Ilyosbekov <suxrobgm@gmail.com>
2026-07-17 21:00:34 -0400

    2210

---
+++ src/CSharp/Solutions/Solution2210.cs
@@ -0,0 +1,37 @@
+﻿namespace LeetCode.Solutions;
+
+public class Solution2210
+{
+    /// <summary>
+    /// 2210. Count Hills and Valleys in an Array - Easy
+    /// <a href="https://leetcode.com/problems/count-hills-and-valleys-in-an-array">See the problem</a>
+    /// </summary>
+    public int CountHillValley(int[] nums)
+    {
+        var count = 0;
+
+        // Track the last value that differs from the current one.
+        var left = nums[0];
+
+        for (var i = 1; i < nums.Length - 1; i++)
+        {
+            // Skip plateaus: equal neighbors are part of the same hill/valley.
+            if (nums[i] == nums[i + 1])
+            {
+                continue;
+            }
+
+            var isHill = nums[i] > left && nums[i] > nums[i + 1];
+            var isValley = nums[i] < left && nums[i] < nums[i + 1];
+
+            if (isHill || isValley)
+            {
+                count++;
+            }
+
+            left = nums[i];
+        }
+
+        return count;
+    }
+}
