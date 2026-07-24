ebbda5139317c43bc910a3bfd7082be7a48a34f4
Sukhrob Ilyosbekov <suxrobgm@gmail.com>
2026-07-23 22:14:42 -0400

    2216

---
+++ src/CSharp/Solutions/Solution2216.cs
@@ -0,0 +1,29 @@
+﻿namespace LeetCode.Solutions;
+
+public class Solution2216
+{
+    /// <summary>
+    /// 2216. Minimum Deletions to Make Array Beautiful - Medium
+    /// <a href="https://leetcode.com/problems/minimum-deletions-to-make-array-beautiful">See the problem</a>
+    /// </summary>
+    public int MinDeletion(int[] nums)
+    {
+        int deletions = 0;
+        int n = nums.Length;
+
+        for (int i = 0; i < n - 1; i++)
+        {
+            if ((i - deletions) % 2 == 0 && nums[i] == nums[i + 1])
+            {
+                deletions++;
+            }
+        }
+
+        if ((n - deletions) % 2 == 1)
+        {
+            deletions++;
+        }
+
+        return deletions;
+    }
+}
