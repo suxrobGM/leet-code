53f56132c16a170c3b4788c50f892762dc864016
Sukhrob Ilyosbekov <suxrobgm@gmail.com>
2026-07-28 12:31:05 -0400

    2221

---
+++ src/CSharp/Solutions/Solution2221.cs
@@ -0,0 +1,23 @@
+﻿namespace LeetCode.Solutions;
+
+public class Solution2221
+{
+    /// <summary>
+    /// 2221. Find Triangular Sum of an Array - Medium
+    /// <a href="https://leetcode.com/problems/find-triangular-sum-of-an-array">See the problem</a>
+    /// </summary>
+    public int TriangularSum(int[] nums)
+    {
+        int n = nums.Length;
+
+        for (int i = 0; i < n - 1; i++)
+        {
+            for (int j = 0; j < n - 1 - i; j++)
+            {
+                nums[j] = (nums[j] + nums[j + 1]) % 10;
+            }
+        }
+
+        return nums[0];
+    }
+}
