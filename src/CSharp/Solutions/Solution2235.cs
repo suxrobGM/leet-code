c1ffda5cf3e389c9c88335e959549abddbbe2164
Sukhrob Ilyosbekov <suxrobgm@gmail.com>
2026-08-07 20:31:24 -0400

    2235

---
+++ src/CSharp/Solutions/Solution2235.cs
@@ -0,0 +1,13 @@
+namespace LeetCode.Solutions;
+
+public class Solution2235
+{
+    /// <summary>
+    /// 2235. Add Two Integers - Easy
+    /// <a href="https://leetcode.com/problems/add-two-integers">See the problem</a>
+    /// </summary>
+    public int Sum(int num1, int num2)
+    {
+        return num1 + num2;
+    }
+}
